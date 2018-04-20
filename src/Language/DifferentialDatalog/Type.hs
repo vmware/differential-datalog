3ee917263971cf6b15529d0011114d591af4c0f9
Leonid Ryzhyk <lryzhyk@vmware.com>
2018-04-20 13:54:15 -0700

    restrict the used of constructors in the LHS of assignment
    type constructor in the LHS of an assignment is only allowed if it
    is the unique constructor of this type

--- src/Language/DifferentialDatalog/Type.hs
+++ src/Language/DifferentialDatalog/Type.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FlexibleContexts, RecordWildCards, TupleSections #-}
 {-
 Copyright (c) 2018 VMware, Inc.
 SPDX-License-Identifier: MIT
@@ -21,6 +20,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 -}
+
+{-# LANGUAGE FlexibleContexts, RecordWildCards, TupleSections #-}
 
 module Language.DifferentialDatalog.Type(
     WithType(..),
