e940f02cd2776ed67973d23322263d8778d6d494
Leonid Ryzhyk <lryzhyk@vmware.com>
2018-09-05 18:39:31 -0700

    progress on tutorial

--- test/datalog_tests/tutorial.rs
+++ test/datalog_tests/tutorial.rs
@@ -4,7 +4,6 @@ fn split_ip_list(x: &String) -> Vec<String> {
     x.as_str().split(" ").map(|x| x.to_string()).collect()
 }
 
-//fn string_slice(x: &String)
 fn string_slice(x: &String, from: &u64, to: &u64) -> String {
     x.as_str()[(*from as usize)..(*to as usize)].to_string()
 }
