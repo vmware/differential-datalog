669e954f365ed0a433cd5084dbe4de97d35899c4
Leonid Ryzhyk <lryzhyk@vmware.com>
2018-10-12 10:24:59 -0700

    progress on JSON parsing

--- rust/template/ovsdb/lib.rs
+++ rust/template/ovsdb/lib.rs
@@ -1,3 +1,5 @@
+//! Parse OVSDB database update messages and convert them into DDlog table update commands
+
 extern crate differential_datalog;
 extern crate serde_json;
 extern crate num;
@@ -8,16 +10,6 @@ use serde_json::map::Map;
 use num::BigInt;
 
 /*
-pub enum Record {
-    Bool(bool),
-    Int(BigInt),
-    String(String),
-    Tuple(Vec<Record>),
-    Array(Vec<Record>),
-    PosStruct(String, Vec<Record>),
-    NamedStruct(String, Vec<(String, Record)>)
-}
-
 pub enum UpdCmd {
     Insert (String, Record),
     Delete (String, Record),
@@ -25,12 +17,19 @@ pub enum UpdCmd {
 }
 */
 
+fn val_into_vec(v: Value) -> Vec<Value> {
+    match v {
+        Value::Array(a) => a,
+        _ => panic!("not an array value")
+    }
+}
+
 fn complain<X>(src: &Value, category: &str) -> Result<X, String>{
     Err(format!("JSON value {} is not a valid {}", src, category))
 }
 
-fn lookup<'a>(m: &'a Map<String, Value>, field: &str) -> Result<&'a Value, String> {
-    m.get(field).ok_or(format!("JSON map does not contain \"{}\" field: {:?}", field, m))
+fn lookup<'a>(m: &'a mut Map<String, Value>, field: &str) -> Result<Value, String> {
+    m.remove(field).ok_or(format!("JSON map does not contain \"{}\" field: {:?}", field, m))
 }
 
 /*
@@ -50,40 +49,45 @@ fn lookup<'a>(m: &'a Map<String, Value>, field: &str) -> Result<&'a Value, Strin
           "cf9dca71-2495-4e61-9fe2-8ea4fb2859cd"]]]
  }
 */
-pub fn cmd_from_val(src: &Value) -> Result<UpdCmd, String> {
+pub fn cmd_from_val(src: Value) -> Result<UpdCmd, String> {
     match src {
-        Value::Object(m) => {
-            let op: String = serde_json::from_value(lookup(m, "op")?.clone()).map_err(|e|e.to_string())?;
-            let table: String = serde_json::from_value(lookup(m,"table")?.clone()).map_err(|e|e.to_string())?;
+        Value::Object(mut m) => {
+            let op: String = serde_json::from_value(lookup(&mut m, "op")?.clone()).map_err(|e|e.to_string())?;
+            let table: String = serde_json::from_value(lookup(&mut m,"table")?.clone()).map_err(|e|e.to_string())?;
             match op.as_ref() {
                 "update" => { 
                     let rec = row_from_val(m, &table)?;
                     Ok(UpdCmd::Insert(table, rec))
                 },
-                _ => Err(format!("unsupported operation {} in {}", op, &src))
+                _ => Err(format!("unsupported operation {} in {:?}", op, m))
             }
         },
         _ => complain(&src, "command")
     }
 }
 
-fn row_from_val(m: &Map<String, Value>, table: &str) -> Result<Record, String> {
+fn row_from_val(mut m: Map<String, Value>, table: &str) -> Result<Record, String> {
     // extract _uuid from "where"-condition of the form
     // "where": [[ "_uuid", "==", ["uuid", "cf9dca71-2495-4e61-9fe2-8ea4fb2859cd"]]]
-    let uuid = match lookup(m, "where")? {
-        Value::Array(v) => {
-            match v.as_slice() {
-                [Value::String(ref field),Value::String(ref op), ref uuid] if field == "_uuid" && op == "==" => {
-                    uuid_from_val(uuid)
+    let uuid = match lookup(&mut m, "where")? {
+        Value::Array(mut v) => {
+            let valid_pattern = match v.as_slice() {
+                [Value::String(field),Value::String(op), Value::Array(_)] => {
+                    field == "_uuid" && op == "=="
                 },
-                _ => Err(format!("unsupported \"where\" condition: {:?}", v))
+                _ => false
+            };
+            if valid_pattern {
+                record_from_array(val_into_vec(v.remove(2)))
+            } else {
+                Err(format!("unsupported \"where\" condition: {:?}", v))
             }
         },
         _ => Err(format!("\"where\" condition is not an array in {:?}", m))
     }?;
 
     // parse row
-    let mut fields = match lookup(m, "row")? {
+    let mut fields = match lookup(&mut m, "row")? {
         Value::Object(obj) => row_from_obj(obj),
         _ => Err(format!("\"row\" is not an object in {:?}", m))
     }?;
@@ -94,18 +98,49 @@ fn row_from_val(m: &Map<String, Value>, table: &str) -> Result<Record, String> {
 /*
     ["uuid", "cf9dca71-2495-4e61-9fe2-8ea4fb2859cd"]
  */
-fn uuid_from_val(src: &Value) -> Result<Record,String> {
-    match src {
-        Value::Array(v) => {
-            match v.as_slice() {
-                [Value::String(ref field), Value::String(ref uuid)] if field == "uuid" => {
-                    Ok(Record::Int(BigInt::parse_bytes(uuid.as_bytes(), 16).
-                                   ok_or(format!("invalid uuid string \"{}\"", uuid))?))
+fn record_from_array(mut src: Vec<Value>) -> Result<Record,String> {
+    if src.len() != 2 {
+        return Err(format!("record array is not of length 2: {:?}", src));
+    };
+    let val = src.remove(1);
+    let field = src.remove(0);
+    match field {
+        Value::String(field) => {
+            match (field.as_str(), val) {
+                ("uuid", Value::String(uuid)) => {
+                    Ok(Record::Int(BigInt::parse_bytes(uuid.as_bytes(), 16)
+                                   .ok_or(format!("invalid uuid string \"{}\"", uuid))?))
                 },
-                _ => Err(format!("invalid uuid format: {:?}", v))
+                ("named-uuid", Value::String(uuid_name)) => {
+                    Ok(Record::String(uuid_name))
+                },
+                ("set", Value::Array(atoms)) => {
+                    let elems: Result<Vec<Record>, String> = atoms.into_iter().map(|a|record_from_val(a)).collect();
+                    Ok(Record::Array(elems?))
+                },
+                ("map", Value::Array(pairs)) => {
+                    let elems: Result<Vec<(Record, Record)>, String> = pairs.into_iter().map(|p|pair_from_val(p)).collect();
+                    Ok(Record::Array(elems?.into_iter().map(|(k,v)|Record::Tuple(vec![k,v])).collect()))
+                },
+                _ => Err(format!("unexpected array value type \"{}\"", field))
             }
+            
         },
-        _ => Err(format!("invalid uuid {}", src))
+        _ => Err(format!("invalid array value: {:?}", src))
+    }
+}
+
+fn pair_from_val(pair: Value) -> Result<(Record, Record), String> {
+    match pair {
+        Value::Array(mut v) => {
+            if v.len() != 2 {
+                return Err(format!("key-value pair must be of length 2: {:?}",v))
+            };
+            let val = v.remove(1);
+            let key = v.remove(0);
+            Ok((record_from_val(key)?, record_from_val(val)?))
+        },
+        _ => Err(format!("invalid key-value pair {}", pair))
     }
 }
 
@@ -115,23 +150,20 @@ fn uuid_from_val(src: &Value) -> Result<Record,String> {
     "name": "br0"
   }
 */
-fn row_from_obj(m: &Map<String, Value>) -> Result<Vec<(String, Record)>, String> {
-    m.iter().map(|(field, val)|Ok((field.clone(), record_from_val(val)?))).collect()
+fn row_from_obj(m: Map<String, Value>) -> Result<Vec<(String, Record)>, String> {
+    m.into_iter().map(|(field, val)|Ok((field, record_from_val(val)?))).collect()
 }
 
-fn record_from_val(v: &Value) -> Result<Record, String> {
+fn record_from_val(v: Value) -> Result<Record, String> {
     match v {
         // <string>
-        Value::String(s) => Ok(Record::String(s.clone())),
+        Value::String(s) => Ok(Record::String(s)),
         // <number>
-        Value::Number(n) if n.is_u64() => Ok(Record::Int(BigInt::from(n.as_u64().unwrap()))),
+        Value::Number(ref n) if n.is_u64() => Ok(Record::Int(BigInt::from(n.as_u64().unwrap()))),
         // <boolean>
-        Value::Bool(b) => Ok(Record::Bool(*b)),
-        // <uuid>
-        // <named-uuid>
-        // <set>
-        // <map>
+        Value::Bool(b) => Ok(Record::Bool(b)),
+        // <uuid>, <named-uuid>, <set>, <map>
+        Value::Array(v) => record_from_array(v),
         _ => Err(format!("unexpected value {}", v))
-    }
-    //Err("not implemented".to_owned())
+    } 
 }
