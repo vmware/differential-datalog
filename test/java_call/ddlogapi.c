a3d3fe9d7b55ad813ebd0812c84cd88be1889cc8
mbudiu-vmw <mbudiu@vmware.com>
2018-11-01 16:26:36 -0700

    Initial draft of Java API (untested)

---
+++ test/java_call/ddlogapi.c
@@ -0,0 +1,242 @@
+/* Wrapper that converts a JNI C interface to calls to the native ddlog C API */
+
+#include <stdlib.h>
+#include <string.h>
+#include "ddlogapi_DDLogAPI.h"
+#include "ddlog.h"
+
+/* The _1 in all the function names below
+   is the JNI translation of the _ character from Java */
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_datalog_1example_1run(
+    JNIEnv *env, jobject obj, jint workers) {
+    if (workers <= 0)
+        workers = 1;
+    return (jlong)datalog_example_run((unsigned)workers);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_datalog_1example_1stop(
+    JNIEnv *env, jobject obj, jlong handle) {
+    return datalog_example_stop((datalog_example_ddlog_prog)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_datalog_1example_1transaction_1start(
+    JNIEnv * env, jobject obj, jlong handle) {
+    return datalog_example_transaction_start((datalog_example_ddlog_prog)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_datalog_1example_1transaction_1rollback(
+    JNIEnv * env, jobject obj, jlong handle) {
+    return datalog_example_transaction_rollback((datalog_example_ddlog_prog)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_datalog_1example_1apply_1updates(
+    JNIEnv * env, jclass obj, jlong value, jobjectArray arr) {
+    // TODO
+    return 0;
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1bool(
+    JNIEnv *env, jclass obj, jboolean b) {
+    return (jlong)ddlog_bool(b);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1u64(
+    JNIEnv *env, jclass obj, jlong l) {
+    return (jlong)ddlog_u64(l);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1string(
+    JNIEnv *env, jclass obj, jstring s) {
+    const char* str = (*env)->GetStringUTFChars(env, s, NULL);
+    ddlog_record* result = ddlog_string(str);
+    (*env)->ReleaseStringUTFChars(env, s, str);
+    return (jlong)result;
+}
+
+jlong create_from_vector(JNIEnv *env, jclass obj, jlongArray handles,
+                         ddlog_record* (*creator)(ddlog_record** fields, size_t size)) {
+     jlong *a = (*env)->GetLongArrayElements(env, handles, NULL);
+     if (a == NULL)
+         return -1;
+     jsize len = (*env)->GetArrayLength(env, handles);
+     ddlog_record** fields = malloc(len * sizeof(ddlog_record*));
+     for (int i = 0; i < len; i++)
+         fields[i] = (ddlog_record*)a[i];
+     ddlog_record* result = creator(fields, (size_t)len);
+     (*env)->ReleaseLongArrayElements(env, handles, a, 0);
+     free(fields);
+     return (jlong)result;
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1tuple(
+    JNIEnv *env, jclass obj, jlongArray handles) {
+    return create_from_vector(env, obj, handles, ddlog_tuple);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1vector(
+    JNIEnv *env, jclass obj, jlongArray handles) {
+    return create_from_vector(env, obj, handles, ddlog_vector);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1set(
+    JNIEnv *env, jclass obj, jlongArray handles) {
+    return create_from_vector(env, obj, handles, ddlog_set);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1map(
+    JNIEnv *env, jclass obj, jlongArray handles) {
+    return create_from_vector(env, obj, handles, ddlog_map);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1pair(
+    JNIEnv *env, jclass obj, jlong h1, jlong h2) {
+    return (jlong)ddlog_pair((ddlog_record*)h1, (ddlog_record*)h2);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1bool(
+    JNIEnv *env, jclass obj, jlong handle) {
+    return (jboolean)ddlog_is_bool((ddlog_record*)handle);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1bool(
+    JNIEnv *env, jclass obj, jlong handle) {
+    return (jboolean)ddlog_get_bool((ddlog_record*)handle);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1int(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jboolean)ddlog_is_int((ddlog_record*)handle);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1u64(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jlong)ddlog_get_u64((ddlog_record*)handle);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1string(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jboolean)ddlog_is_string((ddlog_record*)handle);
+}
+
+const jstring toJString(JNIEnv* env, const char* nonNullStr, size_t size) {
+    char* buf = malloc(size + 1);
+    strncpy(buf, nonNullStr, size);
+    buf[size] = 0;
+    jstring result = (*env)->NewStringUTF(env, buf);
+    free(buf);
+    return result;
+}
+
+JNIEXPORT jstring JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1str(
+    JNIEnv *env, jclass obj, long handle) {
+    const char *s = ddlog_get_str_non_nul((const ddlog_record*)handle);
+    size_t size = ddlog_get_strlen((const ddlog_record*)handle);
+    return toJString(env, s, size);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1tuple(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jboolean)ddlog_is_tuple((ddlog_record*)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1tuple_1size(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jint)ddlog_get_tuple_size((ddlog_record*)handle);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1tuple_1field(
+    JNIEnv *env, jclass obj, jlong handle, jint index) {
+    return (jlong)ddlog_get_tuple_field((ddlog_record*)handle, (size_t)index);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1vector(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jboolean)ddlog_is_vector((ddlog_record*)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1vector_1size(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jint)ddlog_get_vector_size((ddlog_record*)handle);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1vector_1elem(
+    JNIEnv *env, jclass obj, jlong handle, jint index) {
+    return (jlong)ddlog_get_vector_elem((ddlog_record*)handle, (size_t)index);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1set(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jboolean)ddlog_is_set((ddlog_record*)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1set_1size(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jint)ddlog_get_set_size((ddlog_record*)handle);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1set_1elem(
+    JNIEnv *env, jclass obj, jlong handle, jint index) {
+    return (jlong)ddlog_get_set_elem((ddlog_record*)handle, (size_t)index);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1map(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jboolean)ddlog_is_map((ddlog_record*)handle);
+}
+
+JNIEXPORT jint JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1map_1size(
+    JNIEnv *env, jclass obj, long handle) {
+    return (jint)ddlog_get_map_size((ddlog_record*)handle);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1map_1key(
+    JNIEnv *env, jclass obj, jlong handle, jint index) {
+    return (jlong)ddlog_get_map_key((ddlog_record*)handle, (size_t)index);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1map_1val(
+    JNIEnv *env, jclass obj, jlong handle, jint index) {
+    return (jlong)ddlog_get_map_val((ddlog_record*)handle, (size_t)index);
+}
+
+JNIEXPORT jboolean JNICALL Java_ddlogapi_DDLogAPI_ddlog_1is_1pos_1struct(
+    JNIEnv *env, jclass obj, jlong handle) {
+    return (jboolean)ddlog_is_pos_struct((ddlog_record*)handle);
+}
+
+JNIEXPORT jstring JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1constructor(
+    JNIEnv *env, jclass obj, jlong handle) {
+    size_t size;
+    const char *s = ddlog_get_constructor_non_null((const ddlog_record*)handle, &size);
+    return toJString(env, s, size);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1get_1struct_1field(
+    JNIEnv *env, jclass obj, jlong handle, jint index) {
+    return (jlong)ddlog_get_struct_field((ddlog_record*)handle, (size_t)index);
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1insert_1cmd(
+    JNIEnv *env, jclass obj, jstring table, jlong handle) {
+    const char* str = (*env)->GetStringUTFChars(env, table, NULL);
+    ddlog_cmd* result = ddlog_insert_cmd(str, (ddlog_record*)handle);
+    (*env)->ReleaseStringUTFChars(env, table, str);
+    return (jlong)result;
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1delete_1val_1cmd(
+    JNIEnv *env, jclass obj, jstring table, jlong handle) {
+    const char* str = (*env)->GetStringUTFChars(env, table, NULL);
+    ddlog_cmd* result = ddlog_delete_val_cmd(str, (ddlog_record*)handle);
+    (*env)->ReleaseStringUTFChars(env, table, str);
+    return (jlong)result;
+}
+
+JNIEXPORT jlong JNICALL Java_ddlogapi_DDLogAPI_ddlog_1delete_1key_1cmd(
+    JNIEnv *env, jclass obj, jstring table, jlong handle) {
+    const char* str = (*env)->GetStringUTFChars(env, table, NULL);
+    ddlog_cmd* result = ddlog_delete_key_cmd(str, (ddlog_record*)handle);
+    (*env)->ReleaseStringUTFChars(env, table, str);
+    return (jlong)result;
+}
---
+++ test/java_call/ddlogapi/DDLogRecord.java
@@ -0,0 +1,225 @@
+package ddlogapi;
+
+/**
+ * Java wrapper around Differential Datalog C API that manipulates
+ * DDlog data structures.
+ */
+public class DDLogRecord {
+    // This wraps a void* that points to a C ddlog_record object.
+    private long handle;
+    // If true the handle is shared with other objects and should not be
+    // used in some operations.
+    private boolean shared;
+
+    protected DDLogRecord() {
+        // A zero handle indicates an invalid object.
+        this.handle = 0;
+        this.shared = false;
+    }
+
+    /**
+     * Invalidates this container and returns the contained handle.
+     */
+    public long getHandleAndInvalidate() {
+        this.checkHandle();
+        long result = this.handle;
+        this.handle = 0;
+        return result;
+    }
+
+    /**
+     * Deallocate the data from this DDLogRecord.
+     */
+    public void release() {
+        if (this.handle == 0)
+            return;
+        if (!this.shared)
+            DDLogAPI.ddlog_free(this.handle);
+        this.handle = 0;
+        this.shared = false;
+    }
+
+    private void checkHandle() {
+        if (this.handle == 0)
+            throw new RuntimeException("Accessing invalid handle.");
+    }
+
+    private static DDLogRecord fromHandle(long handle) {
+        DDLogRecord result = new DDLogRecord();
+        if (handle == DDLogAPI.error)
+            throw new RuntimeException("Received invalid handle.");
+        result.handle = handle;
+        result.shared = false;
+        return result;
+    }
+
+    /**
+     * Creates an object where the handle is shared with other
+     * objects.
+     */
+    private static DDLogRecord fromSharedHandle(long handle) {
+        DDLogRecord result = fromHandle(handle);
+        result.shared = true;
+        return result;
+    }
+
+    public DDLogRecord(boolean b) {
+        this.handle = DDLogAPI.ddlog_bool(b);
+        this.shared = false;
+    }
+
+    public DDLogRecord(long v) {
+        this.handle = DDLogAPI.ddlog_u64(v);
+        this.shared = false;
+    }
+
+    public DDLogRecord(String s) {
+        this.handle = DDLogAPI.ddlog_string(s);
+        this.shared = false;
+    }
+
+    /**
+     * Creates a pair with the specified fields.
+     */
+    public DDLogRecord(DDLogRecord first, DDLogRecord second) {
+        this.handle = DDLogAPI.ddlog_pair(first.handle, second.handle);
+        this.shared = false;
+    }
+
+    private static long[] getHandles(DDLogRecord[] fields) {
+        long[] handles = new long[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            handles[i] = fields[i].getHandleAndInvalidate();
+        }
+        return handles;
+    }
+
+    public static DDLogRecord makeTuple(DDLogRecord[] fields) {
+        long[] handles = getHandles(fields);
+        return fromHandle(DDLogAPI.ddlog_tuple(handles));
+    }
+
+    public static DDLogRecord makeVector(DDLogRecord[] fields) {
+        long[] handles = getHandles(fields);
+        return fromHandle(DDLogAPI.ddlog_vector(handles));
+    }
+
+    public static DDLogRecord makeSet(DDLogRecord[] fields) {
+        long[] handles = getHandles(fields);
+        return fromHandle(DDLogAPI.ddlog_set(handles));
+    }
+
+    public static DDLogRecord makeStruct(String name, DDLogRecord[] fields) {
+        long[] handles = getHandles(fields);
+        return fromHandle(DDLogAPI.ddlog_struct(name, handles));
+    }
+
+    /**
+     * Creates a map from a vector of pairs.
+     */
+    public static DDLogRecord makeMap(DDLogRecord[] fields) {
+        long[] handles = getHandles(fields);
+        return fromHandle(DDLogAPI.ddlog_map(handles));
+    }
+
+    public boolean getBoolean() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_bool(this.handle))
+            throw new RuntimeException("Value is not boolean");
+        return DDLogAPI.ddlog_get_bool(this.handle);
+    }
+
+    public long getLong() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_int(this.handle))
+            throw new RuntimeException("Value is not long");
+        return DDLogAPI.ddlog_get_u64(this.handle);
+    }
+
+    public int getTupleSize() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_tuple(this.handle))
+            throw new RuntimeException("Value is not a tuple");
+        return DDLogAPI.ddlog_get_tuple_size(this.handle);
+    }
+
+    public DDLogRecord getTupleField(int index) {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_tuple(this.handle))
+            throw new RuntimeException("Value is not a tuple");
+        return fromSharedHandle(DDLogAPI.ddlog_get_tuple_field(this.handle, index));
+    }
+
+    public int getVectorSize() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_vector(this.handle))
+            throw new RuntimeException("Value is not a vector");
+        return DDLogAPI.ddlog_get_vector_size(this.handle);
+    }
+
+    public DDLogRecord getVectorField(int index) {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_vector(this.handle))
+            throw new RuntimeException("Value is not a vector");
+        return fromSharedHandle(DDLogAPI.ddlog_get_vector_elem(this.handle, index));
+    }
+
+    public int getSetSize() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_set(this.handle))
+            throw new RuntimeException("Value is not a set");
+        return DDLogAPI.ddlog_get_set_size(this.handle);
+    }
+
+    public DDLogRecord getSetField(int index) {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_set(this.handle))
+            throw new RuntimeException("Value is not a set");
+        return fromSharedHandle(DDLogAPI.ddlog_get_set_elem(this.handle, index));
+    }
+
+    public int getMapSize() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_map(this.handle))
+            throw new RuntimeException("Value is not a map");
+        return DDLogAPI.ddlog_get_map_size(this.handle);
+    }
+
+    public DDLogRecord getMapKey(int index) {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_map(this.handle))
+            throw new RuntimeException("Value is not a map");
+        return fromSharedHandle(DDLogAPI.ddlog_get_map_key(this.handle, index));
+    }
+
+    public DDLogRecord getMapValue(int index) {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_map(this.handle))
+            throw new RuntimeException("Value is not a map");
+        return fromSharedHandle(DDLogAPI.ddlog_get_map_val(this.handle, index));
+    }
+
+    public String getString() {
+        this.checkHandle();
+        if (!DDLogAPI.ddlog_is_string(this.handle))
+            throw new RuntimeException("Value is not a string");
+        return DDLogAPI.ddlog_get_str(this.handle);
+    }
+
+    public boolean isStruct() {
+        this.checkHandle();
+        return DDLogAPI.ddlog_is_pos_struct(this.handle);
+    }
+
+    public String getStructName() {
+        if (!this.isStruct())
+            throw new RuntimeException("Value is not a struct");
+        return DDLogAPI.ddlog_get_constructor(this.handle);
+    }
+
+    public DDLogRecord getStructField(int index) {
+        if (!this.isStruct())
+            throw new RuntimeException("Value is not a struct");
+        return fromSharedHandle(DDLogAPI.ddlog_get_struct_field(this.handle, index));
+    }
+}
