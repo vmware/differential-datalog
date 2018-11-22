d4eae85e21f0875e1424d7fafb6baf7ba71a346d
Mihai Budiu <mbudiu@vmware.com>
2018-11-22 09:45:50 -0800

    Java api (#116)
    * Initial draft of Java API (untested)
    
    * Added missing function
    
    * Fix sharing bug

---
+++ test/java_call/ddlogapi/DDLogCommand.java
@@ -0,0 +1,40 @@
+package ddlogapi;
+
+public class DDLogCommand {
+    public enum Kind {
+        DeleteVal,
+        DeleteKey,
+        Insert
+    };
+
+    public final Kind kind;
+    public final String table;
+    public final DDLogRecord value;
+
+    public DDLogCommand(final Kind kind, final String table, final DDLogRecord value) {
+        this.kind = kind;
+        this.table = table;
+        this.value = value;
+    }
+
+    /**
+     * Allocates the underlying C data structure representing the command.
+     * At this time the command takes ownership of the value.
+     * Returns a handle to the underlying data structure.
+     */
+    public long allocate() {
+        switch (this.kind) {
+            case DeleteKey:
+                return DDLogAPI.ddlog_delete_key_cmd(
+                        this.table, this.value.getHandleAndInvalidate());
+            case DeleteVal:
+                return DDLogAPI.ddlog_delete_val_cmd(
+                        this.table, this.value.getHandleAndInvalidate());
+            case Insert:
+                return DDLogAPI.ddlog_insert_cmd(
+                        this.table, this.value.getHandleAndInvalidate());
+            default:
+                throw new RuntimeException("Unexpected command " + this.kind);
+        }
+    }
+}
