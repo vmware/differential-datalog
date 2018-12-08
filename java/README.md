380d9d71570186c1ab5cd42ae7429b96a66ec077
Leonid Ryzhyk <lryzhyk@vmware.com>
2018-12-08 12:53:49 -0800

    Reorganize Java bindings
    - move common Java DDlog binding code to ./java (instead of test/java_call).
    - remove "external.c" and everything related to it

---
+++ java/README.md
@@ -0,0 +1,1 @@
+Java bindings for the DDlog API
--- test/datalog_tests/span.sh
+++ test/datalog_tests/span.sh
@@ -4,21 +4,21 @@
 set -ex
 
 # Build the Java library with the Ddlog API
-cd ../java_call
+cd ../../java
 make
-cd ../datalog_tests
-ln -sf ../java_call/ddlogapi .
+cd ../test/datalog_tests
+ln -sf ../../java/ddlogapi .
 # Force linking with the static library by deleting the dynamic library
 rm -f span_ddlog/target/release/libspan_ddlog.so
 # Compile Span.java
-javac -cp ../java_call Span.java
+javac -cp ../../java Span.java
 # Create manifest file for jar
 mkdir -p META-INF
 echo "Main-Class: Span" > META-INF/MANIFEST.MF
 # Create jar containing Span and the Ddlog API
 jar cmvf META-INF/MANIFEST.MF span.jar Span*.class ./ddlogapi/*.class
 # Create a shared library containing all the native code: ddlogapi.c, libspan_ddlog.a
-gcc -shared -Wl,-soname,ddlogapi -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I../../rust/template ../java_call/ddlogapi.c ../java_call/ddlogapi_DDLogAPI.h -Lspan_ddlog/target/release/ -lspan_ddlog -o libddlogapi.so
+gcc -shared -Wl,-soname,ddlogapi -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I../../rust/template ../../java/ddlogapi.c ../../java/ddlogapi_DDLogAPI.h -Lspan_ddlog/target/release/ -lspan_ddlog -o libddlogapi.so
 # Run the java program pointing to the created shared library
 java -Djava.library.path=. -jar span.jar >span.java.dump
 
