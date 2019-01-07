e430d4910ce41cb58ea86c539a4934d334e10e1c
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-01-06 19:51:51 -0800

    more general allocator interface
    - support arbitrary integer types, not just u64
    - allow setting both upper and lower bounds for allocation

--- lib/allocate.rs
+++ lib/allocate.rs
@@ -1,39 +1,67 @@
+extern crate num;
 use std::collections::BTreeSet;
+use std::ops;
+use std::cmp;
 
-pub fn allocate_allocate_u64<B: Ord+Clone>(allocated: &std_Set<u64>, toallocate: &std_Vec<B>, max_val: &u64) -> std_Vec<(B,u64)> {
-    let first = *allocated.x.range(..).next_back().unwrap_or(&0);
-    let mut last = first;
+pub fn allocate_allocate<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
+    allocated: &std_Set<N>,
+    toallocate: &std_Vec<B>,
+    min_val: &N,
+    max_val: &N) -> std_Vec<(B,N)>
+{
+    assert!(*max_val >= *min_val);
+    let range = *max_val - *min_val;
+
+    // Next index to consider
+    let mut next = *allocated.x.range(..).next_back().unwrap_or(&max_val);
+    let mut offset = N::zero();
     let mut res = std_Vec::new();
     for b in toallocate.x.iter() {
         loop {
-            last = if last + 1 <= *max_val {last + 1} else {1};
-            if last == first {
-                return res;
-            };
-            if allocated.x.contains(&last) { continue; };
-            res.x.push(((*b).clone(), last));
-            break;
+            next = if next == *max_val { *min_val } else { next + N::one() };
+            offset = offset + N::one();
+
+            if allocated.x.contains(&next) {
+                if offset == range + N::one() { return res; };
+                continue;
+            } else {
+                res.x.push(((*b).clone(), next));
+                if offset == range + N::one() { return res; };
+                break;
+            }
         }
     };
     return res
 }
 
-pub fn allocate_adjust_allocation_u64<A: Ord+Clone>(allocated: &std_Map<A, u64>, toallocate: &std_Vec<A>, max_val: &u64) -> std_Vec<(A,u64)> {
-    let allocated_ids: BTreeSet<u64> = allocated.x.values().cloned().collect();
-    let first = *allocated_ids.range(..).next_back().unwrap_or(&0);
-    let mut last = first;
+pub fn allocate_adjust_allocation<A: Ord+Clone, N: num::Num+ops::Add+cmp::Ord+Copy>(
+    allocated: &std_Map<A, N>,
+    toallocate: &std_Vec<A>,
+    min_val: &N,
+    max_val: &N) -> std_Vec<(A,N)>
+{
+    assert!(*max_val >= *min_val);
+    let range = *max_val - *min_val;
+
+    let allocated_ids: BTreeSet<N> = allocated.x.values().cloned().collect();
+    let mut next = *allocated_ids.range(..).next_back().unwrap_or(&max_val);
+    let mut offset = N::zero();
     let mut res = std_Vec::new();
     for b in toallocate.x.iter() {
         match allocated.x.get(b) {
             Some(x) => { res.push(((*b).clone(), x.clone())); },
             None => loop {
-                last = if last + 1 <= *max_val {last + 1} else {1};
-                if last == first {
-                    return res;
-                };
-                if allocated_ids.contains(&last) { continue; };
-                res.x.push(((*b).clone(), last));
-                break;
+                next = if next == *max_val { *min_val } else { next + N::one() };
+                offset = offset + N::one();
+
+                if allocated_ids.contains(&next) {
+                    if offset == range + N::one() { return res; };
+                    continue;
+                } else {
+                    res.x.push(((*b).clone(), next));
+                    if offset == range + N::one() { return res; };
+                    break;
+                }
             }
         }
     };
