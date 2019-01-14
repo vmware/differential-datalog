a325bf4767eb8e5353cf6cf3d920538b4c0c5361
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-01-13 21:52:42 -0800

    `allocate_opt()` library function
    The previously existing allocation function only returns successful
    allocations:
    
    ```
    extern function allocate(
        allocated: Set<'N>,
        toallocate: Vec<'B>,
        min_val: 'N,
        max_val: 'N): Vec<('B, 'N)>
    ```
    
    The new one also returns `None` for all remaining items once the address
    space has been exhausted:
    
    ```
    extern function allocate_opt(
        allocated: Set<'N>,
        toallocate: Vec<'B>,
        min_val: 'N,
        max_val: 'N): Vec<('B, Option<'N>)>
    ```

--- lib/allocate.rs
+++ lib/allocate.rs
@@ -38,6 +38,47 @@ pub fn allocate_allocate<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
     return res
 }
 
+pub fn allocate_allocate_opt<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
+    allocated: &std_Set<N>,
+    toallocate: &std_Vec<B>,
+    min_val: &N,
+    max_val: &N) -> std_Vec<(B,std_Option<N>)>
+{
+    assert!(*max_val >= *min_val);
+    let range = *max_val - *min_val;
+
+    // Next index to consider
+    let mut next = *allocated.x.range(..).next_back().unwrap_or(&max_val);
+    // allocated may contain values outside of the [min_val..max_val] range.
+    if next < *min_val || next > *max_val {
+        next = *max_val;
+    };
+    let mut offset = N::zero();
+    let mut res = std_Vec::new();
+    // Signal that address space has been exhausted, but iteration must continue to
+    // assign `None` to all remaining items.
+    let mut exhausted = false;
+    for b in toallocate.x.iter() {
+        loop {
+            if exhausted { 
+                res.x.push(((*b).clone(), std_Option::std_None));
+                break;
+            };
+            if offset == range { exhausted = true; };
+            next = if next == *max_val { *min_val } else { next + N::one() };
+            offset = offset + N::one();
+
+            if allocated.x.contains(&next) {
+                continue;
+            } else {
+                res.x.push(((*b).clone(), std_Option::std_Some{x: next}));
+                break;
+            }
+        }
+    };
+    return res
+}
+
 pub fn allocate_adjust_allocation<A: Ord+Clone, N: num::Num+ops::Add+cmp::Ord+Copy>(
     allocated: &std_Map<A, N>,
     toallocate: &std_Vec<A>,
