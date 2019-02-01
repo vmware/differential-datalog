e5eeb3aef3f1469e16cab3df282264106fe41000
mbudiu-vmw <mbudiu@vmware.com>
2019-02-01 14:29:28 -0800

    Support for 128-bit UUIDs in Java; renamed classes

--- java/test/span.sh
+++ java/test/span.sh
@@ -3,32 +3,33 @@
 
 set -ex
 
-# Compile the span.dl DDlog program; generates
-# ../test/datalog_tests/span_ddlog/target/release/libspan_ddlog.a and .so
-#stack test --ta '-p span'
-ddlog -i ../../test/datalog_tests/span.dl -L../../lib
-pushd ../../test/datalog_tests/span_ddlog
+#stack test --ta '-p span_uuid'
+
+# Compile the span_uuid.dl DDlog program
+ddlog -i ../../test/datalog_tests/span_uuid.dl -L../../lib
+# Compile the rust program; generates ../test/datalog_tests/span_ddlog/target/release/libspan_ddlog.a and .so
+pushd ../../test/datalog_tests/span_uuid_ddlog
 cargo build --release
 popd
-# Build the Java library with the Ddlog API
+# Build the Java library with the DDlog API
 make -C ..
 # Force linking with the static library by deleting the dynamic library
-rm -f ../../test/datalog_tests/span_ddlog/target/release/libspan_ddlog.so
+rm -f ../../test/datalog_tests/span_uuid_ddlog/target/release/libspan_uuid_ddlog.so
 # Compile SpanTest.java
 javac -cp .. SpanTest.java
 # Create manifest file for jar
 mkdir -p META-INF
 echo "Main-Class: SpanTest" > META-INF/MANIFEST.MF
-# Create jar containing SpanTest.* classes and the Ddlog API
+# Create jar containing SpanTest.* classes and the DDlog API
 jar cmvf META-INF/MANIFEST.MF span.jar SpanTest*.class ../ddlogapi/*.class
 rm -rf META-INF
-# Create a shared library containing all the native code: ddlogapi.c, libspan_ddlog.a
-gcc -shared -Wl,-soname,ddlogapi -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I../../rust/template ../ddlogapi.c ../ddlogapi_DDLogAPI.h -L../../test/datalog_tests/span_ddlog/target/release/ -lspan_ddlog -o libddlogapi.so
+# Create a shared library containing all the native code: ddlogapi.c, libspan_uuid_ddlog.a
+gcc -shared -Wl,-soname,ddlogapi -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -I../../rust/template ../ddlogapi.c ../ddlogapi_DDlogAPI.h -L../../test/datalog_tests/span_uuid_ddlog/target/release/ -lspan_uuid_ddlog -o libddlogapi.so
 # Run the java program pointing to the created shared library
-#java -Xcheck:jni -Djava.library.path=. -jar span.jar ../../test/datalog_tests/span.dat >span.java.dump
-java -Djava.library.path=. -jar span.jar ../../test/datalog_tests/span.dat >span.java.dump
+#java -Xcheck:jni -Djava.library.path=. -jar span.jar ../../test/datalog_tests/span_uuid.dat >span_uuid.java.dump
+java -Djava.library.path=. -jar span.jar ../../test/datalog_tests/span_uuid.dat >span_uuid.java.dump
 # Compare outputs
-diff -q span.java.dump ../../test/datalog_tests/span.dump
+diff -q span_uuid.java.dump ../../test/datalog_tests/span_uuid.dump.expected
 
 # Cleanup
 rm -rf META-INF *.class
