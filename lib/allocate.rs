a1957dcb3c6687c6e8dfcd46ddb43303b6621224
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-03-28 10:29:24 -0700

    New library function: `allocate.allocate_with_hint()`
    Allows the caller to provide an optional hint for each requested
    allocation.  The search for available value will start from this
    seed.
    
    This has two purposes:
    - precisely replicate similar behavior in OVN (which is assumed by
      the test suite)
    - improve the performance when good hints are available

--- lib/allocate.rs
+++ lib/allocate.rs
@@ -38,6 +38,47 @@ pub fn allocate_allocate<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
     return res
 }
 
+pub fn allocate_allocate_with_hint<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
+    allocated: &std_Set<N>,
+    toallocate: &std_Vec<(B, std_Option<N>)>,
+    min_val: &N,
+    max_val: &N) -> std_Vec<(B,N)>
+{
+    assert!(*max_val >= *min_val);
+    let range = *max_val - *min_val;
+    let mut new_allocations: BTreeSet<N> = BTreeSet::new();
+
+    // Next index to consider
+    let mut next = *allocated.x.range(..).next_back().unwrap_or(&min_val);
+    // allocated may contain values outside of the [min_val..max_val] range.
+    if next < *min_val || next > *max_val {
+        next = *min_val;
+    };
+    let mut res = std_Vec::new();
+    for (b, hint) in toallocate.x.iter() {
+        let mut offset = N::zero();
+        next = match hint {
+            std_Option::std_None => { next },
+            std_Option::std_Some{x: h} => { *h }
+        };
+        loop {
+            if allocated.x.contains(&next) || new_allocations.contains(&next) {
+                if offset == range { return res; };
+                offset = offset + N::one();
+                next = if next == *max_val { *min_val } else { next + N::one() };
+                continue;
+            } else {
+                res.x.push(((*b).clone(), next));
+                new_allocations.insert(next);
+                if offset == range { return res; };
+                next = if next == *max_val { *min_val } else { next + N::one() };
+                break;
+            }
+        }
+    };
+    return res
+}
+
 pub fn allocate_allocate_opt<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
     allocated: &std_Set<N>,
     toallocate: &std_Vec<B>,
@@ -60,7 +101,7 @@ pub fn allocate_allocate_opt<B: Ord+Clone, N:num::Num+ops::Add+cmp::Ord+Copy>(
     let mut exhausted = false;
     for b in toallocate.x.iter() {
         loop {
-            if exhausted { 
+            if exhausted {
                 res.x.push(((*b).clone(), std_Option::std_None));
                 break;
             };
