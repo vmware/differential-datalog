fa321e1c255c6b785f0fbd39dc0b3a0354388f80
mbudiu-vmw <mbudiu@vmware.com>
2019-04-18 17:04:39 -0700

    Added option to souffle converter to prefix relation names in .dat files

--- test/run-souffle-tests.sh
+++ test/run-souffle-tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Simple shell script which runs part of the souffle tests: just compilation, not Rust execution
 
+set -e
+
 for i in souffle*; do
-    echo $i;
-    cd $i;
-    ../../tools/souffle-converter.py test.dl souffle.dl souffle.dat log && ddlog -i souffle.dl -L ../../lib;
+    echo "Running $i"
+    cd $i
+    ../../tools/souffle-converter.py test.dl souffle.dl souffle.dat && ddlog -i souffle.dl -L ../../lib
     rm -f log
     cd ..
 done
