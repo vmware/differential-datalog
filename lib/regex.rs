bf2e7cb7db59b2a3a7656e529b55dcd3da910725
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-05-02 20:16:46 -0700

    `regex_match()` function
    Returns true if and only if the regex matches the string given
    
    see #190

---
+++ lib/regex.rs
@@ -0,0 +1,9 @@
+extern crate regex;
+
+pub fn regex_regex_match(regex: &String, s: &String) -> bool
+{
+    match regex::Regex::new(regex.as_str()) {
+        Result::Err(_) => false,
+        Result::Ok(re) => re.is_match(&s.as_str())
+    }
+}
