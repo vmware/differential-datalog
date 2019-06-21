4e9ef4379da3345cd9c47e629df20b37afa0aba3
mbudiu-vmw <mbudiu@vmware.com>
2019-06-20 17:14:04 -0700

    Test two programs in the same process

---
+++ java/test2/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Shell script to build and run the a combination of two DDlog programs
+# in the same process
+
+set -ex
+
+if command clang -v 2>/dev/null; then
+    export CC=clang
+else
+    export CC=gcc
+fi
+
+case `uname -s` in
+    Darwin*)    SHLIBEXT=dylib; JDK_OS=darwin;;
+    Linux*)     SHLIBEXT=so; JDK_OS=linux;;
+    *)          echo "Unsupported OS"; exit -1
+esac
+
+# Compile the span_uuid.dl DDlog program
+ddlog -i ./two.dl -L../../lib
+# Compile the rust program; generates two_ddlog/target/debug/two_ddlog.a and .so
+pushd two_ddlog
+cargo build
+popd
+# Build the Java library with the DDlog API
+make -C ..
+# Force linking with the static library by deleting the dynamic library
+rm -f two_ddlog/target/debug/libtwo_ddlog.so
+# Compile TwoTest.java
+javac -cp .. TwoTest.java
+# Create a shared library containing all the native code: ddlogapi.c, libtwo_ddlog.a
+${CC} -shared -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/${JDK_OS} -I../../rust/template ../ddlogapi.c -Ltwo_ddlog/target/debug/ -ltwo_ddlog -o libddlogapi.${SHLIBEXT}
+# Run the java program pointing to the created shared library
+java -Djava.library.path=. -cp ../ddlogapi.jar:. TwoTest
+
+# Cleanup
+rm -rf META-INF *.class
