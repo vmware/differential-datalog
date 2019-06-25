8d1e9b5f4533077647525a2df9234aa8aeccb74c
Remy Wang <remyw@remyw-a01.vmware.com>
2019-06-25 12:34:34 -0700

    mark public functions as unsafe

--- rust/template/differential_datalog/int.rs
+++ rust/template/differential_datalog/int.rs
@@ -97,13 +97,13 @@ pub extern "C" fn int_from_u64(v: u64) -> *mut Int {
 }
 
 #[no_mangle]
-pub extern "C" fn int_from_str(s: *const c_char, radix: u32) -> *mut Int {
+pub unsafe extern "C" fn int_from_str(s: *const c_char, radix: u32) -> *mut Int {
     let c_str = unsafe { CStr::from_ptr(s) };
     Box::into_raw(Box::new(Int::parse_bytes(c_str.to_bytes(), radix)))
 }
 
 #[no_mangle]
-pub extern "C" fn int_free(x: *mut Int) {
+pub unsafe extern "C" fn int_free(x: *mut Int) {
     if x.is_null() {
         return;
     }
