dcdaf5b6c283857eb5ea71ac1b9fe275b2e723c7
Remy Wang <remyw@remyw-a01.vmware.com>
2019-07-08 13:23:12 -0700

    cleanup

--- test/datalog_tests/client/src/main.rs
+++ test/datalog_tests/client/src/main.rs
@@ -1,28 +1,24 @@
 extern crate serde_json;
 
 use ddd_ddlog::api::*;
-use ddd_ddlog::*;
-use differential_datalog::record;
-use std::ffi::CString;
-use std::net::SocketAddr;
+use differential_datalog::record::*;
 use std::borrow::Cow;
+use std::net::SocketAddr;
 
-use tokio::prelude::*;
-use tokio::net::TcpStream;
 use tokio::io;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 
-fn main() -> Result<(), String>{
-    let prog = HDDlog::run(1, false, None::<fn(usize, &record::Record, bool)>);
+fn main() -> Result<(), String> {
+    let prog = HDDlog::run(1, false, None::<fn(usize, &Record, bool)>);
 
-    let b = record::Record::Bool(true);
-
-    // todo assuming the constructor for the record is the same as table name
-    let rec = record::Record::PosStruct(
+    let rec = Record::PosStruct(
         Cow::from("lr.left.CLeft".to_owned()),
-        [b].to_vec());
+        [Record::Bool(false)].to_vec(),
+    );
 
     let table_id = HDDlog::get_table_id("lr.left.CLeft").unwrap();
-    let updates = &[record::UpdCmd::Insert(record::RelIdentifier::RelId(table_id as usize), rec)];
+    let updates = &[UpdCmd::Insert(RelIdentifier::RelId(table_id as usize), rec)];
 
     prog.transaction_start()?;
     prog.apply_updates(updates.into_iter())?;
@@ -32,23 +28,18 @@ fn main() -> Result<(), String>{
     Ok(())
 }
 
-fn transmit(table: libc::size_t,
-            rec: &record::Record,
-            _polarity: bool) {
-
+fn transmit(table: libc::size_t, rec: &Record, _polarity: bool) {
     let addr = "127.0.0.1:8000".parse::<SocketAddr>().unwrap();
     let stream = TcpStream::connect(&addr);
 
     let data = (&*rec, table);
     let s = serde_json::to_string(&data).unwrap();
 
-    let client = stream.and_then(|stream| {
-        io::write_all(stream, s).then( |_result| {
-            Ok(())
-        })
-    }).map_err(|err| {
-        println!("connection error = {:?}", err);
-    });
+    let client = stream
+        .and_then(|stream| io::write_all(stream, s).then(|_result| Ok(())))
+        .map_err(|err| {
+            println!("connection error = {:?}", err);
+        });
 
     tokio::run(client);
 }
--- test/datalog_tests/server/src/main.rs
+++ test/datalog_tests/server/src/main.rs
@@ -2,19 +2,16 @@ extern crate serde_json;
 
 use ddd_ddlog::api::*;
 use ddd_ddlog::*;
-use differential_datalog::record;
-use std::ffi::CString;
-use std::net::SocketAddr;
+use differential_datalog::record::*;
 use std::borrow::Cow;
+use std::net::SocketAddr;
 use std::sync::Arc;
 
+use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
-use tokio::io;
-use std::str;
 
-fn handle_connection(stream: TcpStream, prog: Arc<HDDlog>) -> impl Future<Item = (), Error = ()>
-{
+fn handle_connection(stream: TcpStream, prog: Arc<HDDlog>) -> impl Future<Item = (), Error = ()> {
     let buf = Vec::new();
     let res = io::read_to_end(stream, buf);
 
@@ -22,34 +19,22 @@ fn handle_connection(stream: TcpStream, prog: Arc<HDDlog>) -> impl Future<Item =
         match result {
             Ok((_socket, buf)) => {
                 let s = String::from_utf8(buf).unwrap();
-                // println!("{:?}", &s);
-                let (rec, table): (record::Record, usize)= serde_json::from_str(&s).unwrap();
+                let (rec, table): (Record, usize) = serde_json::from_str(&s).unwrap();
 
-                // let f = record::ddlog_get_struct_field(&rec as *const record::Record, 0);
-
-                if let record::Record::NamedStruct(_, fields) = rec {
-                    // let (_, b) = fields.get(0).unwrap();
-
+                if let Record::NamedStruct(_, fields) = rec {
                     let table_name = relid2name(table).unwrap();
                     let constr = table_name.split('.').last().unwrap();
                     let constr_r = String::from("lr.right.") + constr;
-                    // println!("{:?}", constr_r);
 
-                    //  let bin = CString::new(constr_r).unwrap();
-                    //  let rec = record::ddlog_struct(bin.as_ptr(), [b].as_ptr(), 1);
-
-                    let rec = record::Record::NamedStruct(
-                        Cow::from(constr_r.to_owned()), fields);
-
-                    // println!("{:?}", rec);
+                    let rec = Record::NamedStruct(Cow::from(constr_r.to_owned()), fields);
 
                     let table_id = HDDlog::get_table_id(&constr_r).unwrap();
-                    let updates = &[record::UpdCmd::Insert(record::RelIdentifier::RelId(table_id as usize), rec)];
+                    let updates = &[UpdCmd::Insert(
+                        RelIdentifier::RelId(table_id as usize),
+                        rec)];
                     prog.transaction_start();
                     prog.apply_updates(updates.iter());
                     prog.transaction_commit_dump_changes(Some(show_out));
-
-                    // println!("{:?}", updates);
                 };
             }
             Err(e) => println!("{:?}", e),
@@ -61,31 +46,26 @@ fn handle_connection(stream: TcpStream, prog: Arc<HDDlog>) -> impl Future<Item =
     work
 }
 
-fn show_out(table: usize, rec: &record::Record, polarity: bool) {
+fn show_out(table: usize, rec: &Record, polarity: bool) {
     println!("output is {:?}, {:?}, {:?}", table, rec, polarity);
 }
 
 fn main() {
-
     let addr = "127.0.0.1:8000".parse::<SocketAddr>().unwrap();
     let listener = TcpListener::bind(&addr).unwrap();
 
-    let prog = HDDlog::run(1, false, None::<fn(usize, &record::Record, bool)>);
+    let prog = HDDlog::run(1, false, None::<fn(usize, &Record, bool)>);
+    let prog_p = Arc::new(prog);
 
-    let pm = Arc::new(prog);
-
-    let server = listener.incoming().for_each(move |socket| {
-
-        // prog.transaction_start();
-
-        let work = handle_connection(socket, pm.clone());
-
-        tokio::spawn(work);
-        Ok(())
-    }).map_err(|err| {
-        // Handle error by printing to STDOUT.
-        println!("accept error = {:?}", err);
-    });
+    let server = listener
+        .incoming()
+        .for_each(move |socket| {
+            tokio::spawn(handle_connection(socket, prog_p.clone()));
+            Ok(())
+        })
+        .map_err(|err| {
+            println!("accept error = {:?}", err);
+        });
 
     tokio::run(server);
 }
