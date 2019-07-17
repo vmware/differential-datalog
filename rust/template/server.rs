34d1e59110c746a9c8622f77b38382e667af1b14
Remy Wang <remy.sucre@gmail.com>
2019-07-17 10:57:56 -0700

    Refactor DDlogServer to produce outlets for subscription

--- rust/template/server.rs
+++ rust/template/server.rs
@@ -4,15 +4,40 @@ use differential_datalog::program::{RelId, Update};
 use differential_datalog::record::{Record, UpdCmd, RelIdentifier};
 use api::*;
 use std::iter;
+use std::iter::FromIterator;
+use std::collections::HashSet;
 
 pub struct DDlogServer<O>
 where O: Observer<Update<super::Value>, String>
 {
     prog: HDDlog,
-    observer: O
+    outlets:  Vec<OutLet<O>>
 }
 
-pub struct OutLet;
+impl <O> DDlogServer<O>
+where O: Observer<Update<super::Value>, String>
+{
+    pub fn stream(&mut self, tables: HashSet<RelId>) {
+        let outlet = OutLet{tables : tables, observer : None};
+        self.outlets.push(outlet);
+    }
+}
+
+pub struct OutLet<O>
+where O: Observer<Update<super::Value>, String>
+{
+    tables: HashSet<RelId>,
+    observer: Option<O>
+}
+
+impl <O> Observable<Update<super::Value>, String, O> for OutLet<O>
+where O: Observer<Update<super::Value>, String>
+{
+    fn subscribe(&mut self, observer: O) {
+        self.observer = Some(observer);
+        // TODO more than one subscribers
+    }
+}
 
 impl <O> Observer<Update<super::Value>, String> for DDlogServer<O>
 where O: Observer<Update<super::Value>, String>
@@ -42,9 +67,38 @@ where O: Observer<Update<super::Value>, String>
                 })
             });
 
-            self.observer.on_start();
-            self.observer.on_updates(upds);
-            self.observer.on_commit();
+
+            for outlet in &self.outlets {
+
+                if let Some(observer) = &outlet.observer {
+                    let upds = changes.as_ref().iter().filter_map(|(table_id, table_data)| {
+                        if outlet.tables.contains(table_id) {
+                            Some((table_id, table_data))
+                        } else {
+                            None
+                        }
+                    }).flat_map(|(table_id, table_data)| {
+                        table_data.iter().map(move |(val, weight)| {
+                            debug_assert!(*weight == 1 || *weight == -1);
+                            if *weight == 1 {
+                                Update::Insert{
+                                    relid: *table_id,
+                                    v: val.clone()
+                                }
+                            } else {
+                                Update::DeleteValue{
+                                    relid: *table_id,
+                                    v: val.clone()
+                                }
+                            }
+                        })
+                    });
+
+                    observer.on_start();
+                    observer.on_updates(upds);
+                    observer.on_commit();
+                }
+            }
         }
     }
 
@@ -54,13 +108,5 @@ where O: Observer<Update<super::Value>, String>
 
     fn on_completed(self) {
         self.prog.stop();
-    }
-}
-
-impl <O> Observable<Update<super::Value>, String, O> for DDlogServer<O>
-where O: Observer<Update<super::Value>, String>
-{
-    fn subscribe(&mut self, observer: O) {
-        self.observer = observer;
     }
 }
