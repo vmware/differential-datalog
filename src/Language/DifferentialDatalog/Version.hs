90815740cb78dc10268f75223277495a31abb702
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-07-29 09:09:29 -0700

    v0.7.1 -> v0.7.2

--- src/Language/DifferentialDatalog/Version.hs
+++ src/Language/DifferentialDatalog/Version.hs
@@ -31,7 +31,7 @@ where
 import GitHash
 
 -- Keep this in sync with the binary release version on github
-dDLOG_VERSION = "v0.7.1"
+dDLOG_VERSION = "v0.7.2"
 
 gitHash :: String
 gitHash = giHash $$tGitInfoCwd
