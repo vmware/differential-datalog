5924648e53d532c07e2e01b57abf2054a7196021
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-08-29 16:17:22 -0700

    `mssleep;` command
    A new CLI command that pauses for the specified number of milliseconds.

--- rust/template/src/main.rs
+++ rust/template/src/main.rs
@@ -8,12 +8,15 @@ use std::io::stdout;
 use std::io::Write;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::thread::sleep;
+use std::time::Duration;
 
 use api::{updcmd2upd, HDDlog};
 use cmd_parser::*;
 use datalog_example_ddlog::*;
 use differential_datalog::program::*;
 use differential_datalog::record::*;
+use num_traits::cast::ToPrimitive;
 use rustop::opts;
 use time::precise_time_ns;
 
@@ -134,7 +137,11 @@ fn handle_cmd(
         Command::Echo(txt) => {
             println!("{}", txt);
             Ok(())
-        }
+        },
+        Command::Sleep(ms) => {
+            sleep(Duration::from_millis(ms.to_u64().unwrap()));
+            Ok(())
+        },
         Command::Update(upd, last) => {
             match updcmd2upd(&upd) {
                 Ok(u) => upds.push(u),
