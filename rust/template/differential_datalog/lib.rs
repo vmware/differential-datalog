8c1e7c3ef5ae7075d9d2fbe27978b559d78fd87f
Daniel Mueller <deso@posteo.net>
2019-09-12 15:23:11 -0700

    Move lint white lists into individual crates
    This change splits the global white list of lints we have and moves the
    relevant parts into the individual crates. Doing so developers can now
    easily run clippy for all crates locally (in the same way Travis does)
    by running:
      > cargo clippy --all
    
    This step will make it much faster to fix those issues over time.

--- rust/template/differential_datalog/lib.rs
+++ rust/template/differential_datalog/lib.rs
@@ -1,3 +1,17 @@
+#![allow(
+    clippy::expect_fun_call,
+    clippy::for_kv_map,
+    clippy::get_unwrap,
+    clippy::into_iter_on_ref,
+    clippy::let_and_return,
+    clippy::needless_return,
+    clippy::new_without_default,
+    clippy::ptr_arg,
+    clippy::ptr_offset_with_cast,
+    clippy::should_implement_trait,
+    clippy::type_complexity
+)]
+
 #[macro_use]
 extern crate abomonation;
 extern crate num;
