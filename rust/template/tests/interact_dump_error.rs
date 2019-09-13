bc68aaf11b6b95c94e0420d9a10278c8e67f7845
Daniel Mueller <deso@posteo.net>
2019-09-13 12:40:30 -0700

    Add two basic tests for interact function
    This change adds two very basic tests for the interact function provided
    by the cmd_parser crate and used by CLI applications. I am not sure it's
    the best way to go about testing it, but it's probably better than what
    we had so far.

---
+++ rust/template/tests/interact_dump_error.rs
@@ -0,0 +1,28 @@
+// Note that this file should only contain a single test. The test in
+// here invokes itself and having multiple test running in parallel
+// while that is happening is probably a bad idea.
+
+extern crate cmd_parser;
+
+use cmd_parser::Command;
+
+mod common;
+
+fn handler(command: Command, interactive: bool) -> (i32, bool) {
+    match command {
+        // We return `true` ("continue") on this path and verify that we
+        // actually hit the exit command (i.e., we don't exit when
+        // returning an error.
+        Command::Dump(_) => (-1, true),
+        Command::Exit => (42, false),
+        _ => (0, interactive),
+    }
+}
+
+#[test]
+fn interact_dump_error() {
+    let input = b"\
+dump NonExistantRelation;
+exit;";
+    assert_eq!(common::run_interact_test(input, handler), 42)
+}
