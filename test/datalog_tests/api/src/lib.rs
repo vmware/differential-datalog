b3ce5a224c0ae29d3dd2fc1e9e7ad913a9411f0d
Daniel Mueller <deso@posteo.net>
2019-09-18 14:01:28 -0700

    Integrate api & server_api tests with newly added unit testing feature
    So far the server_api test was to be invoked manually. With this change
    we make use of the previously added DDLog Rust unit testing capability
    to be able to run it via `stack test`, like so:
      > $ stack test --ta '--pattern api'
      > differential-datalog-0.1.0.0: test (suite: differential-datalog-test, args: --pattern api)
      >
      > ddlog tests
      >   parser tests
      >     api:                             OK (0.18s)
      >     server_api:                      OK (0.23s)
      >   compiler tests
      >     api:                             OK (1.84s)
      >     server_api:                      OK (2.01s)
      >   unit tests
      >     ./test/datalog_tests/api:        OK (15.63s)
      >     ./test/datalog_tests/server_api: OK (0.32s)
      >
      > All 6 tests passed (15.63s)
      >
      > differential-datalog-0.1.0.0: Test suite differential-datalog-test passed

---
+++ test/datalog_tests/api/src/lib.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod tests {
+    use api_ddlog::api::*;
+    use api_ddlog::*;
+    use differential_datalog::program::Update;
+    use differential_datalog::record::*;
+    use std::borrow::Cow;
+
+    #[test]
+    fn start_stop() -> Result<(), String> {
+        let prog = HDDlog::run(1, false, |_, _: &Record, _| {});
+
+        // the update consists of inserting a single bool
+        let table = HDDlog::get_table_id("Rin").unwrap();
+        let b = Record::Bool(false);
+        let rec = Record::PosStruct(Cow::from("Rin"), [b].to_vec());
+        let mut updates = Vec::new();
+        updates.push(Update::Insert {
+            relid: 0,
+            v: relval_from_record(table, &rec).unwrap(),
+        });
+
+        // an entire transaction. changes execute on commit
+        prog.transaction_start()?;
+        prog.apply_valupdates(updates.into_iter())?;
+        prog.transaction_commit_dump_changes()?;
+
+        prog.stop()
+    }
+}
