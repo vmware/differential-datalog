879aebc728e46106914aacf494268c42084d6fb7
Daniel Mueller <deso@posteo.net>
2019-09-18 14:01:28 -0700

    Integrate api & server_api tests with newly added unit testing feature
    So far the server_api test was to be invoked manually. With this change
    we make use of the previously added DDLog Rust unit testing capability
    to be able to run it via `stack test`, like so:
      > $ stack test --ta '--pattern api'
      > differential-datalog-0.1.0.0: test (suite: differential-datalog-test, args: --pattern api)
      >
      > ddlog tests
      >   parser tests
      >     api:                             OK (0.18s)
      >     server_api:                      OK (0.23s)
      >   compiler tests
      >     api:                             OK (1.84s)
      >     server_api:                      OK (2.01s)
      >   unit tests
      >     ./test/datalog_tests/api:        OK (15.63s)
      >     ./test/datalog_tests/server_api: OK (0.32s)
      >
      > All 6 tests passed (15.63s)
      >
      > differential-datalog-0.1.0.0: Test suite differential-datalog-test passed

---
+++ test/datalog_tests/server_api/src/lib.rs
@@ -0,0 +1,70 @@
+#[cfg(test)]
+mod tests {
+    use differential_datalog::record::{Record, RelIdentifier, UpdCmd};
+    use observe::{Observable, Observer};
+
+    use server_api_ddlog::api::*;
+    use server_api_ddlog::server;
+    use server_api_ddlog::Relations::*;
+
+    use std::collections::{HashMap, HashSet};
+    use std::sync::{Arc, Mutex};
+
+    #[test]
+    fn subscribe() -> Result<(), String> {
+        // Construct left server with no redirect
+        let prog1 = HDDlog::run(1, false, |_, _: &Record, _| {});
+        let mut s1 = server::DDlogServer::new(prog1, HashMap::new());
+
+        // Construct right server, redirect Up table
+        let prog2 = HDDlog::run(1, false, |_, _: &Record, _| {});
+        let mut redirect2 = HashMap::new();
+        redirect2.insert(lr_left_Up, lr_right_Up);
+        let s2 = server::DDlogServer::new(prog2, redirect2);
+
+        // Stream Up table from left server
+        let mut tables = HashSet::new();
+        tables.insert(lr_left_Up);
+        let mut stream = s1.add_stream(tables);
+
+        // Right server subscribes to the stream
+        let s2 = Arc::new(Mutex::new(s2));
+        let sub = {
+            let s2_a = server::ADDlogServer(s2.clone());
+            stream.subscribe(Box::new(s2_a))
+        };
+
+        // Insert `true` to Left in left server
+        let rec = Record::Bool(true);
+        let table_id = RelIdentifier::RelId(lr_left_Left as usize);
+        let updates = &[UpdCmd::Insert(table_id, rec)];
+
+        // Execute and transmit the update
+        s1.on_start()?;
+        s1.on_updates(Box::new(
+            updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+        ))?;
+        s1.on_commit()?;
+        s1.on_completed()?;
+
+        // Test `unsubscribe`
+        sub.unsubscribe();
+
+        let rec2 = Record::Bool(true);
+        let table_id2 = RelIdentifier::RelId(lr_left_Left as usize);
+        let updates2 = &[UpdCmd::Delete(table_id2, rec2)];
+
+        s1.on_start()?;
+        s1.on_updates(Box::new(
+            updates2.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+        ))?;
+        s1.on_commit()?;
+        s1.on_completed()?;
+
+        // Shutdown and clean up resources
+        s1.remove_stream(stream);
+        s1.shutdown()?;
+        s2.lock().unwrap().shutdown()?;
+        Ok(())
+    }
+}
