be0ae144bb4cbada48d4d9e414ad1f03026421a6
mbudiu-vmw <mbudiu@vmware.com>
2019-09-19 09:25:37 -0700

    Fix the demo

--- java/webserver/run.sh
+++ java/webserver/run.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Tomcat version 9.X does not seem to work correctly as an embedded web server
 TOMCATVERSION="8.5.2"
-TOMCATLIB=`pwd`/apache-tomcat-${TOMCATVERSION}/lib
+TOMCATLIB=$(pwd)/apache-tomcat-${TOMCATVERSION}/lib
 PROG=reach
 
 stack install
@@ -14,10 +14,10 @@ else
     export CC=gcc
 fi
 
-case `uname -s` in
+case $(uname -s) in
     Darwin*)    SHLIBEXT=dylib; OPEN=open; JDK_OS=darwin;;
     Linux*)     SHLIBEXT=so; OPEN=xdg-open; JDK_OS=linux;;
-    *)          echo "Unsupported OS"; exit -1
+    *)          echo "Unsupported OS"; exit 1
 esac
 
 if [ ! -d apache-tomcat-${TOMCATVERSION} ]; then
@@ -45,12 +45,12 @@ ddlog -i ${PROG}.dl -j -L../../lib
 (cd ${PROG}_ddlog; cargo build --features=flatbuf)
 
 # Create a shared library containing all the native code: ddlogapi.c, lib${PROG}_ddlog.a
-${CC} -shared -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/${JDK_OS} -I../../rust/template -I../../lib ../ddlogapi.c -L${PROG}_ddlog/target/debug/ -l${PROG}_ddlog -o libddlogapi.${SHLIBEXT}
+${CC} -shared -fPIC -I"${JAVA_HOME}"/include -I"${JAVA_HOME}"/include/${JDK_OS} -I../../rust/template -I../../lib ../ddlogapi.c -L${PROG}_ddlog/target/debug/ -l${PROG}_ddlog -o libddlogapi.${SHLIBEXT}
 
-CLASSPATH=`pwd`/${PROG}_ddlog/flatbuf/java:`pwd`/../ddlogapi.jar:$CLASSPATH
+CLASSPATH=$(pwd)/${PROG}_ddlog/flatbuf/java:$(pwd)/../ddlogapi.jar:$CLASSPATH
 # Compile generated Java classes (the FlatBuffer Java package must be compiled first and must be in the
 # $CLASSPATH)
-(cd ${PROG}_ddlog/flatbuf/java && javac `ls ddlog/__${PROG}/*.java` && javac `ls ddlog/${PROG}/*.java`)
+(cd ${PROG}_ddlog/flatbuf/java && javac $(ls ddlog/__${PROG}/*.java) && javac $(ls ddlog/${PROG}/*.java))
 
 set +x
 for L in ${TOMCATLIB}/*.jar; do
@@ -64,6 +64,6 @@ ${OPEN} http://localhost:8082/index.html
 java -Djava.library.path=. Main
 
 # Cleanup generated files
-rm -rf *.class tomcat.8082 libddlogapi.so
+rm -rf ./*.class tomcat.8082 libddlogapi.so
 # You can also delete the following
 # rm -rf reach_ddlog apache-tomcat-${TOMCATVERSION}
