74089750b8050e7625c9b4c2c38382829d32a076
mbudiu-vmw <mbudiu@vmware.com>
2019-09-20 10:00:21 -0700

    Do the exception in the standard way

--- java/ddlogapi/DDlogException.java
+++ java/ddlogapi/DDlogException.java
@@ -1,13 +1,7 @@
 package ddlogapi;
 
 public class DDlogException extends Exception {
-    String msg;
-
     public DDlogException(String msg) {
-        this.msg = msg;
-    }
-
-    public String toString() {
-        return "DDlogException: " + msg;
+        super(msg);
     }
 }
