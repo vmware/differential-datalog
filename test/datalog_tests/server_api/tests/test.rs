86789456ad972d7db9e9383ea2cf97e2efeacd00
Daniel Mueller <deso@posteo.net>
2019-09-23 12:06:23 -0700

    Add tests for on_* observable call backs
    The Observer's on_{start,next,commit,completed,error} methods are to
    be invoked when certain events happen on the Observable. This change
    adds tests for that. It also fixes a problem where on_commit was not
    invoked properly on the Observer when no updates were made between
    on_start and on_commit on the Observable.

---
+++ test/datalog_tests/server_api/tests/test.rs
@@ -0,0 +1,235 @@
+use differential_datalog::record::{Record, RelIdentifier, UpdCmd};
+use observe::Observable;
+use observe::Observer;
+use observe::SharedObserver;
+
+use server_api_ddlog::api::*;
+use server_api_ddlog::server;
+use server_api_ddlog::Relations::*;
+
+use std::cell::Cell;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use maplit::hashmap;
+use maplit::hashset;
+
+#[derive(Clone)]
+struct Mock {
+    called_on_start: usize,
+    called_on_commit: usize,
+    called_on_next: usize,
+    called_on_completed: usize,
+    called_on_error: Cell<usize>,
+}
+
+impl Mock {
+    fn new() -> Self {
+        Self {
+            called_on_start: 0,
+            called_on_commit: 0,
+            called_on_next: 0,
+            called_on_completed: 0,
+            called_on_error: Cell::new(0),
+        }
+    }
+}
+
+impl<T, E> Observer<T, E> for Mock
+where
+    T: Send,
+    E: Send,
+{
+    fn on_start(&mut self) -> Result<(), E> {
+        self.called_on_start += 1;
+        Ok(())
+    }
+
+    fn on_commit(&mut self) -> Result<(), E> {
+        self.called_on_commit += 1;
+        Ok(())
+    }
+
+    fn on_next(&mut self, _: T) -> Result<(), E> {
+        self.called_on_next += 1;
+        Ok(())
+    }
+
+    fn on_completed(&mut self) -> Result<(), E> {
+        self.called_on_completed += 1;
+        Ok(())
+    }
+
+    fn on_error(&self, _: E) {
+        self.called_on_error.set(self.called_on_error.get() + 1);
+    }
+}
+
+/// Verify that `on_commit` is called even if we haven't received any
+/// updates.
+#[test]
+fn start_commit_on_no_updates() -> Result<(), String> {
+    let program = HDDlog::run(1, false, |_, _: &Record, _| {});
+    let mut server = server::DDlogServer::new(program, HashMap::new());
+
+    let observable = SharedObserver::new(Mock::new());
+    let mut stream = server.add_stream(hashset! {});
+    stream.subscribe(Box::new(observable.clone()));
+
+    server.on_start()?;
+    server.on_commit()?;
+    server.on_completed()?;
+
+    {
+        let mock = observable.0.lock().unwrap();
+        assert_eq!(mock.called_on_start, 1);
+        assert_eq!(mock.called_on_commit, 1);
+    }
+
+    server.shutdown()?;
+
+    // Also verify that on_completed is called as part of the shutdown
+    // procedure.
+    {
+        let mock = observable.0.lock().unwrap();
+        assert_eq!(mock.called_on_completed, 1);
+        assert_eq!(mock.called_on_error.get(), 0);
+    }
+    Ok(())
+}
+
+/// Verify that we receive an `on_updates` (and `on_next`) call when we
+/// get an update.
+#[test]
+fn start_commit_with_updates() -> Result<(), String> {
+    let program = HDDlog::run(1, false, |_, _: &Record, _| {});
+    let mut server = server::DDlogServer::new(program, HashMap::new());
+
+    let observable = SharedObserver::new(Mock::new());
+    let mut stream = server.add_stream(hashset! {P1Out});
+    stream.subscribe(Box::new(observable.clone()));
+
+    let updates = &[UpdCmd::Insert(
+        RelIdentifier::RelId(P1In as usize),
+        Record::String("test".to_string()),
+    )];
+
+    server.on_start()?;
+    server.on_updates(Box::new(
+        updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+    ))?;
+    server.on_commit()?;
+    server.on_completed()?;
+
+    let mock = observable.0.lock().unwrap();
+    assert_eq!(mock.called_on_start, 1);
+    assert_eq!(mock.called_on_next, 1);
+    assert_eq!(mock.called_on_commit, 1);
+    assert_eq!(mock.called_on_error.get(), 0);
+    Ok(())
+}
+
+/// Verify that we do not receive repeated `on_next` calls for inserts &
+/// deletes of the same object within a single transaction.
+#[test]
+fn multiple_mergable_updates() -> Result<(), String> {
+    let program = HDDlog::run(1, false, |_, _: &Record, _| {});
+    let mut server = server::DDlogServer::new(program, HashMap::new());
+
+    let observable = SharedObserver::new(Mock::new());
+    let mut stream = server.add_stream(hashset! {P1Out});
+    stream.subscribe(Box::new(observable.clone()));
+
+    let updates = &[
+        UpdCmd::Insert(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("42".to_string()),
+        ),
+        UpdCmd::Insert(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("here-to-stay".to_string()),
+        ),
+        UpdCmd::Delete(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("42".to_string()),
+        ),
+    ];
+
+    server.on_start()?;
+    server.on_updates(Box::new(
+        updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+    ))?;
+    server.on_commit()?;
+    server.on_completed()?;
+
+    let mock = observable.0.lock().unwrap();
+    assert_eq!(mock.called_on_start, 1);
+    assert_eq!(mock.called_on_next, 1);
+    assert_eq!(mock.called_on_commit, 1);
+    assert_eq!(mock.called_on_error.get(), 0);
+    Ok(())
+}
+
+/// Check intended behavior in the context of multiple transactions
+/// happening.
+#[test]
+fn multiple_transactions() -> Result<(), String> {
+    let program = HDDlog::run(1, false, |_, _: &Record, _| {});
+    let mut server = server::DDlogServer::new(program, HashMap::new());
+
+    let observable = SharedObserver::new(Mock::new());
+    let mut stream = server.add_stream(hashset! {P1Out});
+    stream.subscribe(Box::new(observable.clone()));
+
+    let updates = &[
+        UpdCmd::Insert(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("first".to_string()),
+        ),
+        UpdCmd::Insert(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("second".to_string()),
+        ),
+    ];
+
+    server.on_start()?;
+    server.on_updates(Box::new(
+        updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+    ))?;
+    server.on_commit()?;
+
+    {
+        let mock = observable.0.lock().unwrap();
+        assert_eq!(mock.called_on_start, 1);
+        assert_eq!(mock.called_on_next, 2);
+        assert_eq!(mock.called_on_commit, 1);
+    }
+
+    let updates = &[
+        UpdCmd::Delete(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("first".to_string()),
+        ),
+    ];
+
+    server.on_start()?;
+    server.on_updates(Box::new(
+        updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+    ))?;
+    server.on_commit()?;
+
+    {
+        let mock = observable.0.lock().unwrap();
+        assert_eq!(mock.called_on_start, 2);
+        assert_eq!(mock.called_on_next, 3);
+        assert_eq!(mock.called_on_commit, 2);
+    }
+
+    server.on_completed()?;
+
+    {
+        let mock = observable.0.lock().unwrap();
+        assert_eq!(mock.called_on_error.get(), 0);
+    }
+    Ok(())
+}
