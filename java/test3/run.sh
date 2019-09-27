a55485f9443b226312f2ef6badf2fd20e577131f
mbudiu-vmw <mbudiu@vmware.com>
2019-09-27 13:33:18 -0700

    Factor out code to compile Java program

--- java/test3/run.sh
+++ java/test3/run.sh
@@ -1,44 +1,14 @@
 #!/bin/bash
 # Shell script to build and run a Java program
-# tied to test/datalog_tests/span_string.dl program
+# tied to ../../test/datalog_tests/span_string.dl program
 
 set -ex
 
-if command clang -v 2>/dev/null; then
-    export CC=clang
-else
-    export CC=gcc
-fi
+source ../build_java.sh
 
-PROG=span_string
-CLASSPATH=$(pwd)/../../test/datalog_tests/${PROG}_ddlog/flatbuf/java:$(pwd)/../ddlogapi.jar:$CLASSPATH
-
-case $(uname -s) in
-    Darwin*)    SHLIBEXT=dylib; JDK_OS=darwin;;
-    Linux*)     SHLIBEXT=so; JDK_OS=linux;;
-    *)          echo "Unsupported OS"; exit 1
-esac
-
-# Compile the ${PROG}.dl DDlog program
-ddlog -i ../../test/datalog_tests/${PROG}.dl -j -L../../lib
-# Compile the rust program; generates ../test/datalog_tests/span_ddlog/target/release/libspan_ddlog.a
-pushd ../../test/datalog_tests/${PROG}_ddlog
-cargo build --release --features=flatbuf
-popd
-
-# Build the Java library with the DDlog API
-make -C ..
-
-# Compile generated Java classes (the FlatBuffer Java package must be compiled first and must be in the
-# $CLASSPATH)
-(cd ../../test/datalog_tests/${PROG}_ddlog/flatbuf/java && javac $(ls ddlog/__"${PROG}"/*.java) && javac $(ls ddlog/"${PROG}"/*.java))
-
-# Compile SpanTest.java
-javac SpanTest.java
-# Create a shared library containing all the native code: ddlogapi.c, lib${PROG}_ddlog.a
-${CC} -shared -fPIC -I"${JAVA_HOME}"/include -I"${JAVA_HOME}"/include/${JDK_OS} -I../../rust/template -I../../lib ../ddlogapi.c -L../../test/datalog_tests/${PROG}_ddlog/target/release/ -l${PROG}_ddlog -o libddlogapi.${SHLIBEXT}
-# Run the java program pointing to the created shared library
+compile ../../test/datalog_tests/span_string.dl SpanTest.java release
 java -Djava.library.path=. SpanTest > spantest.dump
 diff spantest.dump spantest.dump.expected
-# Cleanup generated files
-rm -rf ./*.class libddlogapi.${SHLIBEXT}
+cleanup
+# Additional cleanup
+# rm -f spantest.dump ../../test/datalog_tests/span_string_ddlog
--- java/webserver/run.sh
+++ java/webserver/run.sh
@@ -5,18 +5,10 @@ set -ex
 # Tomcat version 9.X does not seem to work correctly as an embedded web server
 TOMCATVERSION="8.5.2"
 TOMCATLIB=$(pwd)/apache-tomcat-${TOMCATVERSION}/lib
-PROG=reach
-
-stack install
-if command clang -v 2>/dev/null; then
-    export CC=clang
-else
-    export CC=gcc
-fi
 
 case $(uname -s) in
-    Darwin*)    SHLIBEXT=dylib; OPEN=open; JDK_OS=darwin;;
-    Linux*)     SHLIBEXT=so; OPEN=xdg-open; JDK_OS=linux;;
+    Darwin*)    OPEN=open;;
+    Linux*)     OPEN=xdg-open;;
     *)          echo "Unsupported OS"; exit 1
 esac
 
@@ -35,35 +27,20 @@ else
     echo "Tomcat already installed"
 fi
 
-# Build the Java library with the DDlog API.
-make -C ..
-
-# Compile the ${PROG}.dl DDlog program; use -j switch to generate FlatBuffers schema and Java bindings for it
-ddlog -i ${PROG}.dl -j -L../../lib
-
-# Compile the rust program; generates ../test/datalog_tests/${PROG}_ddlog/target/debug/lib${PROG}_ddlog.a
-(cd ${PROG}_ddlog; cargo build --features=flatbuf)
-
-# Create a shared library containing all the native code: ddlogapi.c, lib${PROG}_ddlog.a
-${CC} -shared -fPIC -I"${JAVA_HOME}"/include -I"${JAVA_HOME}"/include/${JDK_OS} -I../../rust/template -I../../lib ../ddlogapi.c -L${PROG}_ddlog/target/debug/ -l${PROG}_ddlog -o libddlogapi.${SHLIBEXT}
-
-CLASSPATH=$(pwd)/${PROG}_ddlog/flatbuf/java:$(pwd)/../ddlogapi.jar:$CLASSPATH
-# Compile generated Java classes (the FlatBuffer Java package must be compiled first and must be in the
-# $CLASSPATH)
-(cd ${PROG}_ddlog/flatbuf/java && javac $(ls ddlog/__${PROG}/*.java) && javac $(ls ddlog/${PROG}/*.java))
-
 set +x
+# This generates too much output
 for L in ${TOMCATLIB}/*.jar; do
     CLASSPATH=$L:$CLASSPATH
 done
-
 set -x
-javac Main.java
+
+source ../build_java.sh
+compile reach.dl Main.java debug
 
 ${OPEN} http://localhost:8082/index.html
 java -Djava.library.path=. Main
 
-# Cleanup generated files
-rm -rf ./*.class tomcat.8082 libddlogapi.so
+cleanup
+rm -rf tomcat.8082
 # You can also delete the following
 # rm -rf reach_ddlog apache-tomcat-${TOMCATVERSION}
