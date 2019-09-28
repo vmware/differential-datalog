6da717da98e7b67670be2af82a499f45c174762c
Daniel Mueller <deso@posteo.net>
2019-09-28 14:00:23 -0700

    Convert delta test into a macro to make it usable for testing different setups
    In the future we want to reuse the test for transferring deltas between
    servers with different observer-observable implementations. In order to
    do so we need to make it somewhat generic. However, because we need to
    use a static in there for accumulating the deltas and checking them
    afterwards, simply passing in a generic function is not enough (multiple
    tests would run in parallel and reuse the static storage).
    So with this change we make this core logic of the test part of a macro
    that various tests can use for their purposes.

--- test/datalog_tests/server_api/tests/deltas.rs
+++ test/datalog_tests/server_api/tests/deltas.rs
@@ -1,53 +1,72 @@
 use std::sync::Mutex;
 
+use differential_datalog::program::Update;
 use differential_datalog::record::{Record, RelIdentifier, UpdCmd};
 use observe::Observable;
 use observe::Observer;
+use observe::SharedObserver;
 
 use server_api_ddlog::api::*;
-use server_api_ddlog::server;
 use server_api_ddlog::Relations::*;
+use server_api_ddlog::server::*;
+use server_api_ddlog::Value;
 
 use lazy_static::lazy_static;
 
 use maplit::hashmap;
 use maplit::hashset;
 
+macro_rules! DeltaTest {
+    ( $($setup_fn:tt)* ) => {{
+        lazy_static! {
+            static ref DELTAS: Mutex<Vec<(usize, Record)>> = {
+                Mutex::new(Vec::new())
+            };
+        }
+
+        fn collect_deltas(relation_id: usize, record: &Record, _weight: isize) {
+            DELTAS.lock().unwrap().push((relation_id, record.clone()));
+        }
+
+        let program1 = HDDlog::run(1, false, |_, _: &Record, _| {});
+        let mut server1 = DDlogServer::new(program1, hashmap! {});
+
+        let program2 = HDDlog::run(1, false, collect_deltas);
+        let server2 = DDlogServer::new(program2, hashmap! {P1Out => P2In});
+
+        let mut stream = server1.add_stream(hashset! {P1Out});
+        ($($setup_fn)*)(&mut stream, SharedObserver::new(server2))?;
+
+        let updates = &[UpdCmd::Insert(
+            RelIdentifier::RelId(P1In as usize),
+            Record::String("delta-me-now".to_string()),
+        )];
+
+        server1.on_start()?;
+        server1.on_updates(Box::new(
+            updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
+        ))?;
+        server1.on_commit()?;
+
+        let deltas = DELTAS.lock().unwrap();
+        let actual = deltas.as_slice();
+        let expected = &[(P2Out as usize, Record::String("delta-me-now".to_string()))];
+        assert_eq!(actual, expected);
+        Ok(())
+    }}
+}
+
 /// Verify that deltas from one program are reported properly in another
 /// that is observing.
 #[test]
-fn single_delta() -> Result<(), String> {
-    lazy_static! {
-        static ref DELTAS: Mutex<Vec<(usize, Record)>> = { Mutex::new(Vec::new()) };
+fn single_delta_direct() -> Result<(), String> {
+    fn do_test(
+        observable: &mut dyn Observable<Update<Value>, String>,
+        observer: SharedObserver<DDlogServer>,
+    ) -> Result<(), String> {
+        observable.subscribe(Box::new(observer));
+        Ok(())
     }
 
-    fn collect_deltas(relation_id: usize, record: &Record, _weight: isize) {
-        DELTAS.lock().unwrap().push((relation_id, record.clone()));
-    }
-
-    let program1 = HDDlog::run(1, false, |_, _: &Record, _| {});
-    let mut server1 = server::DDlogServer::new(program1, hashmap! {});
-
-    let program2 = HDDlog::run(1, false, collect_deltas);
-    let server2 = server::DDlogServer::new(program2, hashmap! {P1Out => P2In});
-
-    let mut stream = server1.add_stream(hashset! {P1Out});
-    let _ = stream.subscribe(Box::new(server2));
-
-    let updates = &[UpdCmd::Insert(
-        RelIdentifier::RelId(P1In as usize),
-        Record::String("delta-me-now".to_string()),
-    )];
-
-    server1.on_start()?;
-    server1.on_updates(Box::new(
-        updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap()),
-    ))?;
-    server1.on_commit()?;
-
-    let deltas = DELTAS.lock().unwrap();
-    let actual = deltas.as_slice();
-    let expected = &[(P2Out as usize, Record::String("delta-me-now".to_string()))];
-    assert_eq!(actual, expected);
-    Ok(())
+    DeltaTest!(do_test)
 }
