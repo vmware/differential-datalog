127e7622e2746478141c3b2a97eefbb383d1e8d6
Daniel Mueller <deso@posteo.net>
2019-09-29 11:00:54 -0700

    Enable "delta" test for TCP channels
    This change hooks up the existing "delta" test for also testing a TCP
    channel backed implementation. We have to adjust the test function in
    two ways to make this work:
    1) it needs to poll for the expected result to be visible because the
       handling of events sent through a TCP channel is not happening
       synchronously from the sender's point of view
    2) we need to support passing out some data created as part of the setup
       to prevent the connection from closing prematurely

--- test/datalog_tests/server_api/tests/deltas.rs
+++ test/datalog_tests/server_api/tests/deltas.rs
@@ -1,20 +1,26 @@
+use std::any::Any;
 use std::sync::Mutex;
+use std::time::Duration;
 
 use differential_datalog::program::Update;
 use differential_datalog::record::{Record, RelIdentifier, UpdCmd};
 use observe::Observable;
 use observe::Observer;
 use observe::SharedObserver;
+use tcp_channel::TcpReceiver;
+use tcp_channel::TcpSender;
 
 use server_api_ddlog::api::*;
-use server_api_ddlog::Relations::*;
 use server_api_ddlog::server::*;
+use server_api_ddlog::Relations::*;
 use server_api_ddlog::Value;
 
 use lazy_static::lazy_static;
 
 use maplit::hashmap;
 use maplit::hashset;
+
+use waitfor::wait_for;
 
 macro_rules! DeltaTest {
     ( $($setup_fn:tt)* ) => {{
@@ -35,7 +41,7 @@ macro_rules! DeltaTest {
         let server2 = DDlogServer::new(program2, hashmap! {P1Out => P2In});
 
         let mut stream = server1.add_stream(hashset! {P1Out});
-        ($($setup_fn)*)(&mut stream, SharedObserver::new(server2))?;
+        let _data = ($($setup_fn)*)(&mut stream, SharedObserver::new(server2))?;
 
         let updates = &[UpdCmd::Insert(
             RelIdentifier::RelId(P1In as usize),
@@ -48,10 +54,17 @@ macro_rules! DeltaTest {
         ))?;
         server1.on_commit()?;
 
-        let deltas = DELTAS.lock().unwrap();
-        let actual = deltas.as_slice();
-        let expected = &[(P2Out as usize, Record::String("delta-me-now".to_string()))];
-        assert_eq!(actual, expected);
+        let result = wait_for::<_, _, ()>(Duration::from_secs(5), Duration::from_millis(10), || {
+            let deltas = DELTAS.lock().unwrap();
+            if deltas.is_empty() {
+                Ok(None)
+            } else {
+                Ok(Some(deltas.clone()))
+            }
+        });
+
+        let expected = vec![(P2Out as usize, Record::String("delta-me-now".to_string()))];
+        assert_eq!(result, Ok(Some(expected)));
         Ok(())
     }}
 }
@@ -63,9 +76,32 @@ fn single_delta_direct() -> Result<(), String> {
     fn do_test(
         observable: &mut dyn Observable<Update<Value>, String>,
         observer: SharedObserver<DDlogServer>,
-    ) -> Result<(), String> {
+    ) -> Result<Box<dyn Any>, String> {
         observable.subscribe(Box::new(observer));
-        Ok(())
+        Ok(Box::new(()))
+    }
+
+    DeltaTest!(do_test)
+}
+
+/// Verify that deltas from one program are reported properly in another
+/// that is observing, using a TCP channel to transfer them over.
+#[test]
+fn single_delta_tcp() -> Result<(), String> {
+    fn do_test(
+        observable: &mut dyn Observable<Update<Value>, String>,
+        observer: SharedObserver<DDlogServer>,
+    ) -> Result<Box<dyn Any>, String> {
+        let mut recv = TcpReceiver::new("127.0.0.1:0").unwrap();
+        let send = TcpSender::new(*recv.addr()).unwrap();
+
+        recv.subscribe(Box::new(observer));
+        observable.subscribe(Box::new(send));
+
+        // We need to pass our receiver out so that it doesn't get
+        // dropped immediately, which would result in a broken
+        // connection.
+        Ok(Box::new(recv))
     }
 
     DeltaTest!(do_test)
