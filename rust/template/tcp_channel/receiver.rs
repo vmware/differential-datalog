da943868c7ce0439c5ae04b79c273b2242a7675c
Daniel Mueller <deso@posteo.net>
2019-10-01 12:23:56 -0700

    Properly join stream processing thread in TcpReceiver::drop
    While perhaps not strictly necessary, this change makes sure that we
    join the thread that the TcpReceiver uses for accepting a sender
    connecting and processing data from it when we destroy the object. Doing
    so can ultimately help finding shutdown related problems which could
    also point to bigger problems. Furthermore, we continue upon message
    deserialization errors.

--- rust/template/tcp_channel/receiver.rs
+++ rust/template/tcp_channel/receiver.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 use std::io::BufReader;
+use std::io::Error;
 use std::net::SocketAddr;
 use std::net::TcpListener;
 use std::net::ToSocketAddrs;
@@ -14,9 +15,10 @@ use std::thread::JoinHandle;
 use bincode::deserialize_from;
 
 use libc::close;
+use libc::shutdown;
+use libc::SHUT_RDWR;
 
 use observe::Observable;
-use observe::Observer;
 use observe::ObserverBox;
 use observe::Subscription;
 use observe::UpdatesSubscription;
@@ -25,15 +27,54 @@ use serde::de::DeserializeOwned;
 
 use crate::message::Message;
 
+#[derive(Copy, Clone, Debug)]
+enum Fd {
+    /// We are still listening for an incoming connection and
+    /// this is the corresponding file descriptor.
+    Listening(RawFd),
+    /// We have accepted a connection and read data from it.
+    Accepted(RawFd),
+    /// The listener/accepted connection has been closed.
+    Closed,
+}
+
+impl Fd {
+    fn close(&mut self) -> Result<(), Error> {
+        match *self {
+            Fd::Listening(fd) | Fd::Accepted(fd) => {
+                let rc = unsafe { shutdown(fd, SHUT_RDWR) };
+                if rc != 0 {
+                    return Err(Error::last_os_error());
+                }
+
+                // Bad luck if we fail the close. There is not much we
+                // can do about that.
+                *self = Fd::Closed;
+
+                let rc = unsafe { close(fd) };
+                if rc != 0 {
+                    return Err(Error::last_os_error());
+                }
+                Ok(())
+            }
+            Fd::Closed => Ok(()),
+        }
+    }
+}
+
 /// The receiving end of a TCP channel has an address
 /// and streams data to an observer.
 #[derive(Debug)]
 pub struct TcpReceiver<T> {
+    /// The address we are listening on.
     addr: SocketAddr,
-    listener: RawFd,
-    stream: Arc<Mutex<Option<RawFd>>>,
-    thread: JoinHandle<Result<(), String>>,
-    observer: Arc<Mutex<Option<Box<dyn Observer<T, String> + Send>>>>,
+    /// Our listener/connection file descriptor state; shared with the
+    /// thread accepting connections and reading streamed data.
+    fd: Arc<Mutex<Fd>>,
+    /// Handle to the thread accepting a connection and processing data.
+    thread: Option<JoinHandle<Result<(), String>>>,
+    /// The connected observer, if any.
+    observer: Arc<Mutex<Option<ObserverBox<T, String>>>>,
 }
 
 impl<T> TcpReceiver<T>
@@ -60,15 +101,13 @@ where
         let addr = listener
             .local_addr()
             .map_err(|e| format!("failed to inquire local address: {}", e))?;
-        let listener_fd = listener.as_raw_fd();
-        let stream_fd = Arc::new(Mutex::new(None));
+        let fd = Arc::new(Mutex::new(Fd::Listening(listener.as_raw_fd())));
         let observer = Arc::new(Mutex::new(None));
-        let thread = Self::accept(stream_fd.clone(), listener, observer.clone());
+        let thread = Some(Self::accept(listener, fd.clone(), observer.clone()));
 
         Ok(Self {
             addr,
-            listener: listener_fd,
-            stream: stream_fd,
+            fd,
             thread,
             observer,
         })
@@ -78,20 +117,49 @@ where
     /// it, and dispatch that to the subscribed observer, if any. If no
     /// observer is subscribed, data will be silently dropped.
     fn accept(
-        stream: Arc<Mutex<Option<RawFd>>>,
         listener: TcpListener,
-        observer: Arc<Mutex<Option<Box<dyn Observer<T, String> + Send>>>>,
+        fd: Arc<Mutex<Fd>>,
+        observer: Arc<Mutex<Option<ObserverBox<T, String>>>>,
     ) -> JoinHandle<Result<(), String>> {
         spawn(move || {
-            let (socket, _) = listener
-                .accept()
-                .map_err(|e| format!("failed to accept connection: {}", e))?;
-            *stream.lock().unwrap() = Some(socket.as_raw_fd());
+            let socket = match listener.accept() {
+                Ok((s, _)) => {
+                    let mut guard = fd.lock().unwrap();
+                    // The user may have closed the receiver shortly
+                    // after us accepting a connection. If that is the
+                    // case do not continue.
+                    if let Fd::Closed = *guard {
+                        return Ok(());
+                    }
+                    *guard = Fd::Accepted(s.as_raw_fd());
+                    s
+                }
+                Err(e) => {
+                    // If the stream has been closed errors are expected
+                    // and we just return to terminate the thread. We
+                    // could alternatively check for a specific error
+                    // return that occurs when the listener socket is
+                    // closed concurrently but that seems less portable.
+                    if let Fd::Closed = *fd.lock().unwrap() {
+                        return Ok(());
+                    } else {
+                        return Err(format!("failed to accept connection: {}", e));
+                    }
+                }
+            };
 
             let mut reader = BufReader::new(socket);
             loop {
-                let message = deserialize_from(&mut reader)
-                    .map_err(|e| format!("failed to deserialize message: {}", e))?;
+                let message = match deserialize_from(&mut reader) {
+                    Ok(m) => m,
+                    Err(_) => {
+                        if let Fd::Closed = *fd.lock().unwrap() {
+                            return Ok(());
+                        }
+                        // TODO: Can/should we log the error?
+                        continue;
+                    }
+                };
 
                 // If there is no observer we just drop the data, which
                 // is seemingly the only reasonable behavior given that
@@ -122,14 +190,17 @@ where
 
 impl<T> Drop for TcpReceiver<T> {
     fn drop(&mut self) {
-        // We can't really handle any errors here.
-        // The order of operations is important. First close the
-        // listener to be sure that a no new stream will be accepted,
-        // then close the stream itself.
-        unsafe {
-            let _ = close(self.listener);
-            let _ = self.stream.lock().unwrap().map(|fd| close(fd));
-        }
+        // TODO: We probably want to just log failures.
+        self.fd.lock().unwrap().close().unwrap();
+        // TODO: We probably want to log any errors reported by the
+        //       thread being joined.
+        self.thread
+            .take()
+            .map(JoinHandle::join)
+            .unwrap()
+            .unwrap()
+            .unwrap();
+
         // The remaining members will be destroyed automatically, no
         // need to bother here.
     }
