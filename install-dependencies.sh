4c8307bbfd6c6f1f277c5bb63822516ac3800c02
mbudiu-vmw <mbudiu@vmware.com>
2019-10-07 13:45:11 -0700

    Run rustfmt before committing

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,9 +17,15 @@ echo "Installing Haskell"
 echo "Installing Rust"
 curl https://sh.rustup.rs -sSf | sh -s - -y
 export PATH=$HOME/.cargo/bin:$PATH
+rustup component add rustfmt
+rustup component add clippy
 
 ./tools/install-flatbuf.sh
 cd flatbuffers
 export CLASSPATH=`pwd`"/java":$CLASSPATH
 export PATH=`pwd`:$PATH
 cd ..
+
+GITDIR=$(git rev-parse --git-dir)
+# Link to pre-commit script
+ln -sf `$(pwd)`/tools/prepush.sh ${GITDIR}/hooks/pre-push
