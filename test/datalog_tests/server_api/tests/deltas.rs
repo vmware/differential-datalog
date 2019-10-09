ecfd1761f32e099b30a6a01d4dbf4feb6e2822b6
Daniel Mueller <deso@posteo.net>
2019-10-09 10:55:28 -0700

    Use lazy_static in deltas test
    This change adjusts the deltas test to use the lazy_static macro instead
    of rolling our own initialization logic with a bunch of unsafe in it.

--- test/datalog_tests/server_api/tests/deltas.rs
+++ test/datalog_tests/server_api/tests/deltas.rs
@@ -8,6 +8,8 @@ use server_api_ddlog::api::*;
 use server_api_ddlog::server;
 use server_api_ddlog::Relations::*;
 
+use lazy_static::lazy_static;
+
 use maplit::hashmap;
 use maplit::hashset;
 
@@ -15,18 +17,12 @@ use maplit::hashset;
 /// that is observing.
 #[test]
 fn single_delta() -> Result<(), String> {
-    // We need a global here (and cannot, say, close over some local
-    // from a lambda expression) because our callback has to implement
-    // Clone and that is not possible for a closure capturing its
-    // environment in a mutable way.
-    // We need an option because Vec::new is not const and so we can't
-    // use it to initialize a static.
-    static mut DELTAS: Option<Mutex<Vec<(usize, Record)>>> = None;
-    unsafe { DELTAS = Some(Mutex::new(Vec::new())) };
+    lazy_static! {
+        static ref DELTAS: Mutex<Vec<(usize, Record)>> = { Mutex::new(Vec::new()) };
+    }
 
     fn collect_deltas(relation_id: usize, record: &Record, _weight: isize) {
-        let deltas = unsafe { DELTAS.as_mut().unwrap() };
-        deltas.lock().unwrap().push((relation_id, record.clone()));
+        DELTAS.lock().unwrap().push((relation_id, record.clone()));
     }
 
     let program1 = HDDlog::run(1, false, |_, _: &Record, _| {});
@@ -49,8 +45,7 @@ fn single_delta() -> Result<(), String> {
     ))?;
     server1.on_commit()?;
 
-    let mutex = unsafe { DELTAS.as_mut().unwrap() };
-    let deltas = mutex.lock().unwrap();
+    let deltas = DELTAS.lock().unwrap();
     let actual = deltas.as_slice();
     let expected = &[(P2Out as usize, Record::String("delta-me-now".to_string()))];
     assert_eq!(actual, expected);
