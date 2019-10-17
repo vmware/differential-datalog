a4c8ddca9fad817d4a746a754d2fc65a8fbab6c7
Daniel Mueller <deso@posteo.net>
2019-10-17 11:08:12 -0700

    Introduce OptionalObserver
    This change introduces another concrete type of (wrapping) observer:
    OptionalObserver. Objects of this type may or may not have an actual
    observer set currently, and if that is not the case any events relayed
    to it are just silently ignored.

--- rust/template/distributed_datalog/src/lib.rs
+++ rust/template/distributed_datalog/src/lib.rs
@@ -1,6 +1,7 @@
 pub use observe::Observable;
 pub use observe::Observer;
 pub use observe::ObserverBox;
+pub use observe::OptionalObserver;
 pub use observe::SharedObserver;
 pub use observe::UpdatesObservable;
 pub use tcp_channel::TcpReceiver;
