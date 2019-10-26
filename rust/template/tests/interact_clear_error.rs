8058a9475a57e42ff7473527c2d7da6f702e45c0
Daniel Mueller <deso@posteo.net>
2019-10-26 09:24:44 -0700

    Do not exit CLI on 'clear' errors
    Some time back we adjusted the CLI to not exit on every error. For
    example, misspelling a relation to the 'dump' command previously
    resulted in exit of the CLI, loosing all the state in there.
    For some reason we missed doing the same for the 'clear' command. So
    this patch makes up for that.

---
+++ rust/template/tests/interact_clear_error.rs
@@ -0,0 +1,23 @@
+// Note that this file should only contain a single test. The test in
+// here invokes itself and having multiple test running in parallel
+// while that is happening is probably a bad idea.
+
+use cmd_parser::Command;
+
+mod common;
+
+fn handler(command: Command, interactive: bool) -> (Result<(), String>, bool) {
+    match command {
+        Command::Clear(_) => (Err("unexpected clear command".to_string()), true),
+        Command::Exit => (Ok(()), false),
+        _ => (Err("unexpected command".to_string()), interactive),
+    }
+}
+
+#[test]
+fn interact_clear_error() {
+    let input = b"\
+clear NonExistantRelation;
+exit;";
+    assert_eq!(common::run_interact_test(input, handler), Ok(()))
+}
