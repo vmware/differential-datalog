8f12a5b0063dfca62369e9264966356b51b66d0c
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-11-06 10:02:43 -0800

    Run `rustfmt` in `lib`.

--- lib/log.rs
+++ lib/log.rs
@@ -1,6 +1,6 @@
-use std::sync;
 use std::collections;
 use std::ffi;
+use std::sync;
 
 type log_callback_t = Box<dyn Fn(log_log_level_t, &str) + Send + Sync>;
 
@@ -17,8 +17,7 @@ lazy_static! {
  * Logging API exposed to the DDlog program.
  * (see detailed documentation in `log.dl`)
  */
-pub fn log_log(module: &log_module_t, level: &log_log_level_t, msg: &String) -> bool
-{
+pub fn log_log(module: &log_module_t, level: &log_log_level_t, msg: &String) -> bool {
     if let Some((cb, current_level)) = LOG_CONFIG.read().unwrap().get(&module) {
         if *level <= *current_level {
             cb(*level, msg.as_str());
@@ -36,12 +35,15 @@ pub fn log_log(module: &log_module_t, level: &log_log_level_t, msg: &String) -> 
  * NOTE: we set callback and log level simultaneously.  A more flexible API
  * would allow changing log level without changing the callback.
  */
-pub fn log_set_callback(module: log_module_t, cb: Option<log_callback_t>, max_level: log_log_level_t)
-{
+pub fn log_set_callback(
+    module: log_module_t,
+    cb: Option<log_callback_t>,
+    max_level: log_log_level_t,
+) {
     match cb {
         Some(cb) => {
             LOG_CONFIG.write().unwrap().insert(module, (cb, max_level));
-        },
+        }
         None => {
             LOG_CONFIG.write().unwrap().remove(&module);
         }
@@ -52,27 +54,24 @@ pub fn log_set_callback(module: log_module_t, cb: Option<log_callback_t>, max_le
  * C bindings for the config API
  */
 #[no_mangle]
-pub unsafe extern "C" fn ddlog_log_set_callback(module: raw::c_int,
-                                                cb: Option<extern "C" fn(arg: libc::uintptr_t,
-                                                                         level: raw::c_int,
-                                                                         msg: *const raw::c_char)>,
-                                                cb_arg: libc::uintptr_t,
-                                                max_level: raw::c_int)
-{
+pub unsafe extern "C" fn ddlog_log_set_callback(
+    module: raw::c_int,
+    cb: Option<extern "C" fn(arg: libc::uintptr_t, level: raw::c_int, msg: *const raw::c_char)>,
+    cb_arg: libc::uintptr_t,
+    max_level: raw::c_int,
+) {
     match cb {
-        Some(cb) => {
-            log_set_callback(module as log_module_t,
-                             Some(Box::new(move |level, msg| {
-                                 cb(cb_arg,
-                                    level as raw::c_int,
-                                    ffi::CString::new(msg).unwrap_or_default().as_ptr())
-                             })),
-                             max_level as log_log_level_t)
-        },
-        None => {
-            log_set_callback(module as log_module_t,
-                             None,
-                             max_level as log_log_level_t)
-        }
+        Some(cb) => log_set_callback(
+            module as log_module_t,
+            Some(Box::new(move |level, msg| {
+                cb(
+                    cb_arg,
+                    level as raw::c_int,
+                    ffi::CString::new(msg).unwrap_or_default().as_ptr(),
+                )
+            })),
+            max_level as log_log_level_t,
+        ),
+        None => log_set_callback(module as log_module_t, None, max_level as log_log_level_t),
     }
 }
