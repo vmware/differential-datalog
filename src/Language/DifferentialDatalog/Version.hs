d53f285290b7ae483907ea61a53770837b090370
Leonid Ryzhyk <lryzhyk@vmware.com>
2019-11-18 09:30:54 -0800

    DDlog v0.11.0
    - Switch to DD release 0.11.0
    - Cleanup some unused dependencies in `Cargo.toml`
    - Bump version to 0.11.0

--- src/Language/DifferentialDatalog/Version.hs
+++ src/Language/DifferentialDatalog/Version.hs
@@ -32,7 +32,7 @@ import GitHash
 
 -- Keep this in sync with the binary release version on github
 dDLOG_VERSION :: String
-dDLOG_VERSION = "v0.10.5"
+dDLOG_VERSION = "v0.11.0"
 
 gitHash :: String
 gitHash = giHash $$tGitInfoCwd
