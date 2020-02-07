f1960576337d254d7b02031a3b89c93f4746f086
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-02-06 16:17:38 -0800

    IP address manipulation library.
    Three new library types:
    `net.IpAddr`   - IPv4 or IPv6 address
    `net.Ipv4Addr` - IPv4 address
    `net.Ipv6Addr` - IPv6 address

---
+++ lib/net/README.md
@@ -0,0 +1,7 @@
+# Networking primitives for TCP/UDP communication.
+
+This library provides networking functionality for the Transmission Control
+and User Datagram Protocols, as well as types for IP and socket addresses.
+
+Functions and data types in this library are bindings for functions and data
+types in the `std::net` Rust module.
---
+++ test/datalog_tests/test-libs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Test DDlog libraries. 
+
+set -e
+
+./run-test.sh lib_test.dl release
+
+# $1 - test name
+test_lib() {
+    echo Running $1 test
+    /usr/bin/time ./lib_test_ddlog/target/release/lib_test_cli --no-print < $1.dat > $1.dump
+    diff -q $1.dump.expected $1.dump
+}
+
+test_lib uuid_test
+test_lib net_test
