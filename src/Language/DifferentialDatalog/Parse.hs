cd91e4dc780da0367b08fd88b4eb9bbb3038dd4f
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-02-06 16:14:24 -0800

    Parse.hs: Forbid the use of `self` as identifier.
    It clashes with Rust keyword.
    
    Ideally, we should be renaming such identifiers, but we don't have
    the infrastructure to do this at the moment.

--- src/Language/DifferentialDatalog/Parse.hs
+++ src/Language/DifferentialDatalog/Parse.hs
@@ -59,7 +59,7 @@ parseDatalogString program file = do
 
 -- The following Rust keywords are declared as Datalog keywords to
 -- prevent users from declaring variables with the same names.
-rustKeywords = ["type", "match"]
+rustKeywords = ["type", "match", "self"]
 
 reservedOpNames = [":", "|", "&", "==", "=", ":-", "%", "*", "/", "+", "-", ".", "->", "=>", "<=",
                    "<=>", ">=", "<", ">", "!=", ">>", "<<", "~", "@", "#"]
