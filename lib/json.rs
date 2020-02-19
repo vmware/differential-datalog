ceb01482f05411e9cf0e0dee900e7e664d12d3e0
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-02-18 23:07:22 -0800

    json.dl: JsonScalarValue type.
    Extend the `json.dl` library with an enum type that represents a scalar JSON
    value whose type is not known in advance.  This requires a bit of Rust
    magic to work as expected; therefore it makes sense to implement this as
    a library type.

--- lib/json.rs
+++ lib/json.rs
@@ -7,3 +7,31 @@ pub fn json_from_json_string<'de, T: serde::Deserialize<'de>>(
 pub fn json_to_json_string<T: serde::Serialize>(x: &T) -> std_Result<String, String> {
     res2std(serde_json::to_string(x))
 }
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ScalarValueInner {
+    ValString(String),
+    ValBool(bool),
+    ValInt(i64),
+}
+
+impl From<ScalarValueInner> for json_JsonScalarValue {
+    fn from(x: ScalarValueInner) -> Self {
+        match x {
+            ScalarValueInner::ValString(s) => json_JsonScalarValue::json_ValString { s },
+            ScalarValueInner::ValBool(b) => json_JsonScalarValue::json_ValBool { b },
+            ScalarValueInner::ValInt(i) => json_JsonScalarValue::json_ValInt { i },
+        }
+    }
+}
+
+impl From<json_JsonScalarValue> for ScalarValueInner {
+    fn from(x: json_JsonScalarValue) -> Self {
+        match x {
+            json_JsonScalarValue::json_ValString { s } => ScalarValueInner::ValString(s),
+            json_JsonScalarValue::json_ValBool { b } => ScalarValueInner::ValBool(b),
+            json_JsonScalarValue::json_ValInt { i } => ScalarValueInner::ValInt(i),
+        }
+    }
+}
