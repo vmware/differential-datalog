9593499a3a53e37f7dbd8d77b35e785672fed139
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-02-26 22:20:57 -0800

    json.dl: JsonValue type.
    `JsonValue` represents an arbitraty JSON document.  It is handy when
    working with fragments of JSON whose schema is either unknown or is to
    messy that it cannot be easily modeled in DDlog.

--- lib/json.rs
+++ lib/json.rs
@@ -8,30 +8,83 @@ pub fn json_to_json_string<T: serde::Serialize>(x: &T) -> std_Result<String, Str
     res2std(serde_json::to_string(x))
 }
 
-#[derive(Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum ScalarValueInner {
-    ValString(String),
-    ValBool(bool),
-    ValInt(i64),
-}
-
-impl From<ScalarValueInner> for json_JsonScalarValue {
-    fn from(x: ScalarValueInner) -> Self {
+impl From<serde_json::value::Value> for json_JsonValue {
+    fn from(x: serde_json::value::Value) -> Self {
         match x {
-            ScalarValueInner::ValString(s) => json_JsonScalarValue::json_ValString { s },
-            ScalarValueInner::ValBool(b) => json_JsonScalarValue::json_ValBool { b },
-            ScalarValueInner::ValInt(i) => json_JsonScalarValue::json_ValInt { i },
+            serde_json::value::Value::Null => json_JsonValue::json_JsonNull,
+            serde_json::value::Value::Bool(b) => json_JsonValue::json_JsonBool { b },
+            serde_json::value::Value::Number(n) => json_JsonValue::json_JsonNumber {
+                n: json_JsonNum::from(n),
+            },
+            serde_json::value::Value::String(s) => json_JsonValue::json_JsonString { s },
+            serde_json::value::Value::Array(a) => {
+                let v: Vec<json_JsonValue> =
+                    a.into_iter().map(|v| json_JsonValue::from(v)).collect();
+                json_JsonValue::json_JsonArray {
+                    a: std_Vec::from(v),
+                }
+            }
+            serde_json::value::Value::Object(o) => json_JsonValue::json_JsonObject {
+                o: o.into_iter()
+                    .map(|(k, v)| (k, json_JsonValue::from(v)))
+                    .collect(),
+            },
         }
     }
 }
 
-impl From<json_JsonScalarValue> for ScalarValueInner {
-    fn from(x: json_JsonScalarValue) -> Self {
+impl From<json_JsonValue> for serde_json::value::Value {
+    fn from(x: json_JsonValue) -> Self {
         match x {
-            json_JsonScalarValue::json_ValString { s } => ScalarValueInner::ValString(s),
-            json_JsonScalarValue::json_ValBool { b } => ScalarValueInner::ValBool(b),
-            json_JsonScalarValue::json_ValInt { i } => ScalarValueInner::ValInt(i),
+            json_JsonValue::json_JsonNull => serde_json::value::Value::Null,
+            json_JsonValue::json_JsonBool { b } => serde_json::value::Value::Bool(b),
+            json_JsonValue::json_JsonNumber { n } => {
+                serde_json::value::Value::Number(serde_json::Number::from(n))
+            }
+            json_JsonValue::json_JsonString { s } => serde_json::value::Value::String(s),
+            json_JsonValue::json_JsonArray { a } => serde_json::value::Value::Array(
+                a.into_iter()
+                    .map(|v| serde_json::value::Value::from(v))
+                    .collect(),
+            ),
+            json_JsonValue::json_JsonObject { o } => serde_json::value::Value::Object(
+                o.into_iter()
+                    .map(|(k, v)| (k, serde_json::value::Value::from(v)))
+                    .collect(),
+            ),
+        }
+    }
+}
+
+impl From<serde_json::Number> for json_JsonNum {
+    fn from(n: serde_json::Number) -> Self {
+        if n.is_u64() {
+            json_JsonNum::json_JsonInt {
+                i: n.as_u64().unwrap() as i128,
+            }
+        } else if n.is_i64() {
+            json_JsonNum::json_JsonInt {
+                i: n.as_i64().unwrap() as i128,
+            }
+        } else if n.is_f64() {
+            json_JsonNum::json_JsonFloat
+        } else {
+            panic!("JsonNum::from::<Number>(): unknown number format: '{}'", n)
+        }
+    }
+}
+
+impl From<json_JsonNum> for serde_json::Number {
+    fn from(n: json_JsonNum) -> Self {
+        match n {
+            json_JsonNum::json_JsonInt { i } => {
+                if i >= 0 {
+                    serde_json::Number::from(i as u64)
+                } else {
+                    serde_json::Number::from(i as i64)
+                }
+            }
+            json_JsonNum::json_JsonFloat => unimplemented!("JsonFloat"),
         }
     }
 }
