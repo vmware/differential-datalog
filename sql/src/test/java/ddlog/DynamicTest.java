be1a0ccd1ff5d7e3eb8fd03a75c2dc31479ed07b
Mihai Budiu <mbudiu@vmware.com>
2020-04-01 17:25:30 -0700

    Removed redundant code; robustified library

--- sql/src/test/java/ddlog/DynamicTest.java
+++ sql/src/test/java/ddlog/DynamicTest.java
@@ -8,16 +8,14 @@ import org.jooq.impl.DSL;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class DynamicTest {
+public class DynamicTest extends BaseQueriesTest {
     /*
      * Sets up an H2 in-memory database that we use for all tests.
      */
@@ -46,16 +44,13 @@ public class DynamicTest {
 
     @Test
     public void testDynamicLoading() throws IOException, DDlogException, IllegalAccessException, NoSuchFieldException {
-        String ddlogProgram = "input relation R(v: bit<16>)\n" +
+        String ddlogProgram = "import sql\n" + // not needed, but we test the libraries too
+                "import sqlop\n" +
+                "input relation R(v: bit<16>)\n" +
                 "output relation O(v: bit<16>)\n" +
                 "O(v) :- R(v).";
-        String filename = "program.dl";
-        File file = new File(filename);
-        file.deleteOnExit();
-        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-        bw.write(ddlogProgram);
-        bw.close();
-        DDlogAPI api = Translator.compileAndLoad(file.getName());
+        File file = this.writeProgramToTempFile(ddlogProgram);
+        DDlogAPI api = Translator.compileAndLoad(file.toString(), "../lib", "./lib");
         if (api == null)
             throw new RuntimeException("Could not load program");
 
@@ -72,5 +67,6 @@ public class DynamicTest {
         api.transactionStart();
         api.applyUpdates(ca);
         api.transactionCommitDumpChanges(s -> Assert.assertEquals("From 0 Insert O{10}", s.toString()));
+        api.stop();
     }
 }
