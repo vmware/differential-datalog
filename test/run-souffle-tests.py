b1bd45fb061fb0062c367bc60571377773599e0d
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-08-15 22:49:47 -0700

    Disable another souffle test.
    ```
    ddlog: error: souffle.dl:26:78-26:93: Unknown variable: _candidateMonth
    Ragg_0(_candidateMonth, _totalSales) :- RSales(_product, _m, _sales), (_m <= _candidateMonth), var _totalSales = Aggregate((_candidateMonth), group_sum(_sales)).
                                                                                 ^^^^^^^^^^^^^^^
    ```

--- test/run-souffle-tests.py
+++ test/run-souffle-tests.py
@@ -31,6 +31,7 @@ xfail = [
     "aggregates_complex", # cannot be evaluated bottom-up; issue 293
     "aggregates_non_materialised", # Rule must start with a positive literal
     "aggregates",  # issue 227 - count of empty group
+    "aggregates_nested", # ??
     "indexed_inequalities", # DDlog cannot infer the type of `2` in `2^32`.
     "magic_aggregates",  # 227
     "count",       # 227
