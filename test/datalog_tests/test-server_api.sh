f52d3b86bb6ed66a2b3431d21b65b00003fa768d
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-09-14 21:44:05 -0700

    test-server_api: Decrease test iterations to 10.
    I was not able to figure out why gitlab keeps recompiling tests on each
    iteration, so reducing iteration number from 100 to 10 to avoid CI
    timeouts.

--- test/datalog_tests/test-server_api.sh
+++ test/datalog_tests/test-server_api.sh
@@ -11,7 +11,8 @@ TEST_DIR="${THIS_DIR}/server_api"
     cd "${TEST_DIR}" &&
     i=0 &&
     true &&
-    while [ $? -eq 0 -a $i -lt 100 ]; do
+    #while [ $? -eq 0 -a $i -lt 100 ]; do
+    while [ $? -eq 0 -a $i -lt 10 ]; do
         i=$((i+1));
         #CARGO_LOG=cargo::core::compiler::fingerprint=trace RUST_LOG=trace cargo test
         RUST_LOG=trace cargo test
