a77c5a406e69b29e130e7aeb2e46e91bd2532d3d
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-10-05 19:49:32 -0700

    Rename `min_by`, `max_by` -> `arg_min`, `arg_max`.
    Use more standard names for these operations.

--- lib/set.rs
+++ lib/set.rs
@@ -1,14 +1,14 @@
 use crate::closure::Closure;
 use crate::ddlog_std;
 
-pub fn set_min_by<A: Ord + Clone, B: Ord>(
+pub fn set_arg_min<A: Ord + Clone, B: Ord>(
     s: &ddlog_std::Set<A>,
     f: &Box<dyn Closure<*const A, B>>,
 ) -> ddlog_std::Option<A> {
     ddlog_std::Option::from(s.x.iter().min_by_key(|x| f.call(*x)).map(|x| x.clone()))
 }
 
-pub fn set_max_by<A: Ord + Clone, B: Ord>(
+pub fn set_arg_max<A: Ord + Clone, B: Ord>(
     s: &ddlog_std::Set<A>,
     f: &Box<dyn Closure<*const A, B>>,
 ) -> ddlog_std::Option<A> {
--- lib/vec.rs
+++ lib/vec.rs
@@ -5,14 +5,14 @@ pub fn vec_sort_by<A, B: Ord>(v: &mut ddlog_std::Vec<A>, f: &Box<dyn Closure<*co
     v.x.sort_unstable_by_key(|x| f.call(x))
 }
 
-pub fn vec_min_by<A: Clone, B: Ord>(
+pub fn vec_arg_min<A: Clone, B: Ord>(
     v: &ddlog_std::Vec<A>,
     f: &Box<dyn Closure<*const A, B>>,
 ) -> ddlog_std::Option<A> {
     ddlog_std::Option::from(v.x.iter().min_by_key(|x| f.call(*x)).map(|x| x.clone()))
 }
 
-pub fn vec_max_by<A: Clone, B: Ord>(
+pub fn vec_arg_max<A: Clone, B: Ord>(
     v: &ddlog_std::Vec<A>,
     f: &Box<dyn Closure<*const A, B>>,
 ) -> ddlog_std::Option<A> {
