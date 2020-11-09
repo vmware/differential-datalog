e72c2fe0e59062e43d6629638f6faa81405df1c5
Lalith Suresh <lsuresh@vmware.com>
2020-11-09 15:24:54 -0800

    Support insert queries
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- sql/src/main/java/com/vmware/ddlog/DDlogJooqProvider.java
+++ sql/src/main/java/com/vmware/ddlog/DDlogJooqProvider.java
@@ -5,7 +5,9 @@
 package com.vmware.ddlog;
 
 import ddlogapi.DDlogAPI;
+import ddlogapi.DDlogCommand;
 import ddlogapi.DDlogException;
+import ddlogapi.DDlogRecCommand;
 import ddlogapi.DDlogRecord;
 import org.jooq.DSLContext;
 import org.jooq.Field;
@@ -22,6 +24,7 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class DDlogJooqProvider implements MockDataProvider {
@@ -29,7 +32,8 @@ public class DDlogJooqProvider implements MockDataProvider {
     private static final String STRING_TYPE = "java.lang.String";
     private static final String BOOLEAN_TYPE = "java.lang.Boolean";
     private static final String LONG_TYPE = "java.lang.Long";
-
+    private static final String DDLOG_SOME_TYPE = "ddlog_std::Some";
+    private static final String DDLOG_NONE_TYPE = "ddlog_std::None";
     private final DDlogAPI dDlogAPI;
     private final DSLContext dslContext;
     private final Map<String, List<Field<?>>> tables = new HashMap<>();
@@ -52,9 +56,10 @@ public class DDlogJooqProvider implements MockDataProvider {
         final MockResult[] mock = new MockResult[1];
         // The execute context contains SQL string(s), bind values, and other meta-data
         final String sql = ctx.sql();
-
         if (sql.toUpperCase().startsWith("SELECT")) {
             mock[0] = executeSelectStar(sql);
+        } else if (sql.toUpperCase().startsWith("INSERT INTO")) {
+            mock[0] = executeInsert(sql);
         } else {
             // Exceptions are propagated through the JDBC and jOOQ APIs
             throw new SQLException("Statement not supported: " + sql);
@@ -63,12 +68,12 @@ public class DDlogJooqProvider implements MockDataProvider {
     }
 
     private MockResult executeSelectStar(final String sql) throws SQLException {
-        final String[] s = sql.toUpperCase().split(" ");
-        if (!(s.length == 4 && s[1].equals("*") && s[2].equals("FROM"))) {
+        final String[] s = sql.split("[ ]+");
+        if (!(s.length == 4 && s[1].equalsIgnoreCase("*") && s[2].equalsIgnoreCase("FROM"))) {
             throw new SQLException("Statement not supported: " + sql);
         }
         final String tableName = s[3];
-        final List<Field<?>> fields = tables.get(tableName);
+        final List<Field<?>> fields = tables.get(tableName.toUpperCase());
         if (fields == null) {
             throw new SQLException("Unknown table: " + tableName);
         }
@@ -89,13 +94,40 @@ public class DDlogJooqProvider implements MockDataProvider {
         return new MockResult(1, result);
     }
 
+    private MockResult executeInsert(final String sql) throws SQLException {
+        final String[] s = sql.replaceAll("[(),]", "")
+                              .split("[ ]+");
+        if (!(s.length >= 4 && s[1].equalsIgnoreCase("INTO")
+                && s[3].equalsIgnoreCase("VALUES"))) {
+            throw new SQLException("Statement not supported: " + sql);
+        }
+        final String tableName = s[2];
+        final int valuesIndex = 4; // the index representing the 1st field of the value being inserted
+        final List<Field<?>> fields = tables.get(tableName.toUpperCase());
+        if (fields == null) {
+            throw new SQLException("Table cannot be queried: " + tableName);
+        }
+        String[] valuesTuple = Arrays.copyOfRange(s, valuesIndex, s.length);
+        final DDlogRecord dDlogRecord = toDDlogRecord(tableName, valuesTuple);
+        final int tableId = dDlogAPI.getTableId("R" + tableName.toLowerCase());
+        final DDlogRecCommand command = new DDlogRecCommand(DDlogCommand.Kind.Insert, tableId, dDlogRecord);
+        try {
+            dDlogAPI.transactionStart();
+            dDlogAPI.applyUpdates(new DDlogRecCommand[]{command});
+            dDlogAPI.transactionCommit();
+        } catch (final DDlogException e) {
+            e.printStackTrace();
+        }
+        return new MockResult(1, null);
+    }
+
     @Nullable
     private Object structToValue(final Field<?> field, final DDlogRecord record) {
         final Class<?> cls = field.getType();
-        if (record.isStruct() && record.getStructName().equals("ddlog_std::None")) {
+        if (record.isStruct() && record.getStructName().equals(DDLOG_NONE_TYPE)) {
             return null;
         }
-        if (record.isStruct() && record.getStructName().equals("ddlog_std::Some")) {
+        if (record.isStruct() && record.getStructName().equals(DDLOG_SOME_TYPE)) {
             return structToValue(field, record.getStructField(0));
         }
         switch (cls.getName()) {
@@ -109,6 +141,74 @@ public class DDlogJooqProvider implements MockDataProvider {
                 return record.getString();
             default:
                 throw new RuntimeException("Unknown datatype %s of field %s in table %s in update received");
+        }
+    }
+
+    private DDlogRecord toDDlogRecord(final String tableName, final String[] args) {
+        final DDlogRecord[] recordsArray = new DDlogRecord[args.length];
+        final List<Field<?>> fields = tables.get(tableName.toUpperCase());
+
+        int fieldIndex = 0;
+        for (final Field<?> field : fields) {
+            final Class<?> cls = field.getType();
+            try {
+                // Handle nullable columns here
+                if (args[fieldIndex] == null) {
+                    recordsArray[fieldIndex] = DDlogRecord.makeStruct(DDLOG_NONE_TYPE, new DDlogRecord[0]);
+                }
+                else {
+                    switch (cls.getName()) {
+                        case BOOLEAN_TYPE:
+                            recordsArray[fieldIndex] = maybeOption(field,
+                                    new DDlogRecord(Boolean.parseBoolean(args[fieldIndex])));
+                            break;
+                        case INTEGER_TYPE:
+                            recordsArray[fieldIndex] = maybeOption(field,
+                                    new DDlogRecord(Integer.parseInt(args[fieldIndex])));
+                            break;
+                        case LONG_TYPE:
+                            recordsArray[fieldIndex] = maybeOption(field,
+                                    new DDlogRecord(Long.parseLong(args[fieldIndex])));
+                            break;
+                        case STRING_TYPE:
+                            // Strings have to be escaped by single quotes
+                            if (args[fieldIndex].startsWith("'") && args[fieldIndex].endsWith("'")) {
+                                recordsArray[fieldIndex] = maybeOption(field,
+                                        new DDlogRecord(args[fieldIndex].substring(1, args[fieldIndex].length() - 1)));
+                            } else {
+                                throw new RuntimeException("Unexpected string");
+                            }
+                            break;
+                        default:
+                            throw new RuntimeException(String.format("Unknown datatype %s of field %s in table %s" +
+                                            " while sending DB data to DDLog", args[fieldIndex].getClass().getName(),
+                                    field.getName(), tableName));
+                    }
+                }
+            } catch (final DDlogException e) {
+                throw new RuntimeException(e);
+            }
+            fieldIndex = fieldIndex + 1;
+        }
+
+        try {
+            return DDlogRecord.makeStruct("T" + tableName.toLowerCase(Locale.US), recordsArray);
+        } catch (final DDlogException | NullPointerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public DDlogRecord maybeOption(final Field<?> field, final DDlogRecord record) {
+        if (field.getDataType().nullable()) {
+            try {
+                final DDlogRecord[] arr = new DDlogRecord[1];
+                arr[0] = record;
+                return DDlogRecord.makeStruct(DDLOG_SOME_TYPE, arr);
+            } catch (final DDlogException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return record;
         }
     }
 }
