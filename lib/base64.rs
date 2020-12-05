d8f4f6d1cf883bc944418fab2c8cc75931a9c5db
Mihai Budiu <mbudiu@vmware.com>
2020-12-04 20:52:40 -0800

    Nazi rust linter

--- lib/base64.rs
+++ lib/base64.rs
@@ -1,6 +1,6 @@
-use base64::DecodeError as b64DecodeError;
 use base64::decode as b64decode;
 use base64::encode as b64encode;
+use base64::DecodeError as b64DecodeError;
 
 pub fn encode(s: &String) -> String {
     b64encode(s)
@@ -8,17 +8,19 @@ pub fn encode(s: &String) -> String {
 
 pub fn decode(s: &String) -> crate::ddlog_std::Result<String, String> {
     match b64decode(s) {
-        Ok(r) => crate::ddlog_std::Result::Ok{ res: String::from_utf8(r).unwrap() },
+        Ok(r) => crate::ddlog_std::Result::Ok{
+            res: String::from_utf8(r).unwrap(),
+        },
         Err(e) => match (e) {
-            b64DecodeError::InvalidByte(p, b) => crate::ddlog_std::Result::Err{
-                err: format!("Invalid byte {} at position {}", b, p)
+            b64DecodeError::InvalidByte(p, b) => crate::ddlog_std::Result::Err {
+                err: format!("Invalid byte {} at position {}", b, p),
             },
-            b64DecodeError::InvalidLength => crate::ddlog_std::Result::Err{
-                err: format!("Invalid length")
+            b64DecodeError::InvalidLength => crate::ddlog_std::Result::Err {
+                err: format!("Invalid length"),
             },
-            b64DecodeError::InvalidLastSymbol(p, b) => crate::ddlog_std::Result::Err{
-                err: format!("Invalid last byte {} at position {}", b, p)
-            }
-        }
+            b64DecodeError::InvalidLastSymbol(p, b) => crate::ddlog_std::Result::Err {
+                err: format!("Invalid last byte {} at position {}", b, p),
+            },
+        },
     }
 }
