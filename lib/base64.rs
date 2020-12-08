c87b646f25baee29afff69ab85f5a84bb901b585
Mihai Budiu <mbudiu@vmware.com>
2020-12-07 16:55:36 -0800

    Encode/decode should take vec<u8>

--- lib/base64.rs
+++ lib/base64.rs
@@ -2,14 +2,14 @@ use base64::decode as b64decode;
 use base64::encode as b64encode;
 use base64::DecodeError as b64DecodeError;
 
-pub fn encode(s: &String) -> String {
-    b64encode(s)
+pub fn encode(s: &crate::ddlog_std::Vec<u8>) -> String {
+    b64encode(&s.x)
 }
 
-pub fn decode(s: &String) -> crate::ddlog_std::Result<String, String> {
+pub fn decode(s: &String) -> crate::ddlog_std::Result<crate::ddlog_std::Vec<u8>, String> {
     match b64decode(s) {
-        Ok(r) => crate::ddlog_std::Result::Ok {
-            res: String::from_utf8(r).unwrap(),
+        Ok(r) => crate::ddlog_std::Result::Ok{
+            res: crate::ddlog_std::Vec::from(r),
         },
         Err(e) => match (e) {
             b64DecodeError::InvalidByte(p, b) => crate::ddlog_std::Result::Err {
