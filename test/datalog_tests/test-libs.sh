9ce365fce96073e1d79041a2746ba5e36fe51daf
Mihai Budiu <mbudiu@vmware.com>
2020-12-07 16:55:36 -0800

    Encode/decode should take vec<u8>

--- test/datalog_tests/test-libs.sh
+++ test/datalog_tests/test-libs.sh
@@ -9,7 +9,7 @@ DDLOGFLAGS="-g" ./run-test.sh lib_test.dl release
 # $1 - test name
 test_lib() {
     echo Running $1 test
-    /usr/bin/time ./lib_test_ddlog/target/release/lib_test_cli --no-init-snapshot < $1.dat > $1.dump
+    RUST_BACKTRACE=full /usr/bin/time ./lib_test_ddlog/target/release/lib_test_cli --no-init-snapshot < $1.dat > $1.dump
     diff $1.dump.expected $1.dump
 }
 
