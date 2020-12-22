41f48f6dd28cab77efe915fbeb72f5cd7ae6d25a
Leonid Ryzhyk <lryzhyk@vmware.com>
2020-12-21 17:57:09 -0800

    Use private docker repo in CI.
    Since Dockerhub is no longer usable from private runners, we are now
    hosting the docker image used to run DDlog tests in gitlab in a
    VMware-internal registry.

--- gitlab-ci/build-docker-image.sh
+++ gitlab-ci/build-docker-image.sh
@@ -17,4 +17,6 @@ echo DIR is $DIR
 docker build  --network=host -t ddlog/gitlab-ci:$TAG $DIR/../tools
 echo Successfully created docker image.
 echo To upload the image to dockerhub, run \"docker push ddlog/gitlab-ci:$TAG\".
+echo To upload the image to Harbor, run \"docker tag ddlog/gitlab-ci:$TAG harbor-repo.vmware.com/ddlog/gitlab-ci\" and \"docker push harbor-repo.vmware.com/ddlog/gitlab-ci:$TAG\".
+echo You must be logged into both dockerhub and Harbor using \"docker login\"
 echo To log into the image: \"docker run --network=host -it ddlog/gitlab-ci:$TAG\".
