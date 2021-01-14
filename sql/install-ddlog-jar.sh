3763406173d7a245f92c89c40e40e2e37145cfd6
Mihai Budiu <mbudiu@vmware.com>
2021-01-13 17:47:40 -0800

    Make sure the latest ddlog is used in SQL tests.

--- sql/install-ddlog-jar.sh
+++ sql/install-ddlog-jar.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# When running in CI, the DDlog compiler should be prinstalled by the build stage.
+if [ -z "${IS_CI_RUN}" ]; then
+    stack install
+fi
+
 cd ../java
 make
 mvn install:install-file -Dfile=ddlogapi.jar -DgroupId=ddlog -DartifactId=ddlogapi -Dversion=0.1 -Dpackaging=jar
