bac661d3879857bf170deaa5c28e01039e1f6b94
Leonid Ryzhyk <lryzhyk@vmware.com>
2021-02-07 11:50:12 -0800

    Run twitter microbench as a separate make target.
    Separate twitter microbenchmark into a separate make target.  Also, pass
    the `--bench` flag to `cargo bench` to make sure it only runs one
    benchmark group.  Without this flag, `cargo bench twitter` still runs
    the `main` funcion for the livejournal benchmark, which calls
    `livejournal::dataset()`, which fails if only the twitter dataset has
    been downloaded.

--- rust/ddlog_benches/benches/live_journal.rs
+++ rust/ddlog_benches/benches/live_journal.rs
@@ -14,8 +14,8 @@ fn record_counts() -> impl Iterator<Item = usize> {
     (50_000..=200_000).step_by(50_000)
 }
 
-fn live_journal(c: &mut Criterion) {
-    let mut group = c.benchmark_group("LiveJournal");
+fn live_journal_micro(c: &mut Criterion) {
+    let mut group = c.benchmark_group("LiveJournal-micro");
     group.sampling_mode(SamplingMode::Flat);
     group.sample_size(10);
 
@@ -73,5 +73,5 @@ fn live_journal_macro(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, live_journal, live_journal_macro);
+criterion_group!(benches, live_journal_micro, live_journal_macro);
 criterion_main!(benches);
