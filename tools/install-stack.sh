28f6899847e8c581a4cda8c1d616d6d1733464e0
Mihai Budiu <mbudiu@vmware.com>
2021-03-31 14:39:59 -0700

    Try starting edge

--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -18,7 +18,7 @@ fetch_stack_linux() {
 }
 
 fetch_stack_windows() {
-  curl -L downloads.haskell.org >/dev/null
+  start msedge downloads.haskell.org
   curl -L https://www.stackage.org/stack/windows-x86_64 > stack.exe && unzip stack.exe -d ~/.local/bin
 }
 
