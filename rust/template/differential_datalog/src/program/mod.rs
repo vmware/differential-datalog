5f8c9eab1b29c56721113f4a7773f45d86d9107c
Leonid Ryzhyk <lryzhyk@vmware.com>
2021-06-11 14:31:41 -0700

    Address Rust warning in `program/mod.rs`.
    Rust 1.52+ complains:
    
    ```
    warning: trait objects without an explicit `dyn` are deprecated
        --> differential_datalog/src/program/mod.rs:1500:27
         |
    1500 |                 let one = Any::downcast_ref::<<S::Timestamp as Timestamp>::Summary>(&(1 as TS))
         |                           ^^^ help: use `dyn`: `<dyn Any>`
         |
         = note: `#[warn(bare_trait_objects)]` on by default
    ```
    
    This patch applies the change suggested by the compiler.

--- rust/template/differential_datalog/src/program/mod.rs
+++ rust/template/differential_datalog/src/program/mod.rs
@@ -1498,7 +1498,7 @@ impl Program {
                 ref next,
             } => {
                 #[allow(clippy::unnecessary_cast)]
-                let one = Any::downcast_ref::<<S::Timestamp as Timestamp>::Summary>(&(1 as TS))
+                let one = <dyn Any>::downcast_ref::<<S::Timestamp as Timestamp>::Summary>(&(1 as TS))
                     .expect("Differentiate operator used in recursive context");
 
                 let diff = with_prof_context(&description, || {
