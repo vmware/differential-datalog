5fa7a4c090a23eaaca8263f41cd3d004ff970670
Vikram Narayanan <vikram186@gmail.com>
2021-07-20 12:15:42 -0700

    d3log/broadcast: Bring back the vector clone

--- rust/template/d3log/src/broadcast.rs
+++ rust/template/d3log/src/broadcast.rs
@@ -53,7 +53,7 @@ impl Ingress {
 
 impl Transport for Ingress {
     fn send(&self, b: Batch) {
-        let ports = &*self.broadcast.ports.lock().expect("lock");
+        let ports = &*self.broadcast.ports.lock().expect("lock").clone();
         for (port, index) in ports {
             if *index != self.index {
                 port.send(b.clone())
@@ -64,6 +64,8 @@ impl Transport for Ingress {
 
 impl Transport for Broadcast {
     fn send(&self, b: Batch) {
+        /// We clone this map to have a read-only copy, else, we'd open up the possiblity of a
+        /// deadlock, if this `send` forms a cycle.
         let ports = &*self.ports.lock().expect("lock").clone();
         for (port, _) in ports {
             port.send(b.clone())
