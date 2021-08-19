cad28f2e4c3577f351c2199a5ff301687b4c1d2c
Mihai Budiu <mbudiu@vmware.com>
2021-08-18 18:11:27 -0700

    Add copyright, fix documentation

--- java/streams/src/main/java/org/dbsp/algebraic/IntegerGroup.java
+++ java/streams/src/main/java/org/dbsp/algebraic/IntegerGroup.java
@@ -21,37 +21,29 @@
  * SOFTWARE.
  */
 
-package org.dbsp.formal;
-
-import org.dbsp.algebraic.TimeFactory;
-import org.dbsp.types.IStream;
-import org.dbsp.types.StreamType;
-import org.dbsp.types.Type;
-
-import java.util.function.Function;
+package org.dbsp.algebraic;
 
 /**
- * An operator that works on streams.  It delays the input stream by 1 clock.
- * @param <T> Concrete implementation of elements of the input and output stream.
+ * The group of integer values.
+ * This implementation uses finite Java integers.
  */
-public class DelayOperator<T, F extends TimeFactory> extends
-        UniformUnaryOperator<IStream<T>> {
-    final Type<T> elementType;
+public class IntegerGroup implements Group<Integer> {
+    private IntegerGroup() {}
 
-    public DelayOperator(Type<T> type, F factory) {
-        super(new StreamType<T>(type, factory));
-        this.elementType = type;
-        if (!type.isStream())
-            throw new RuntimeException("Delay must be applied to a stream type, not to " + type);
+    public static final IntegerGroup instance = new IntegerGroup();
+
+    @Override
+    public Integer minus(Integer data) {
+        return -data;
     }
 
     @Override
-    public Function<IStream<T>, IStream<T>> getComputation() {
-        return (IStream<T> s) -> s.delay(this.elementType.getGroup());
+    public Integer add(Integer left, Integer right) {
+        return left + right;
     }
 
     @Override
-    public String toString() {
-        return "z";
+    public Integer zero() {
+        return 0;
     }
 }
--- java/streams/src/main/java/org/dbsp/formal/UnaryOperator.java
+++ java/streams/src/main/java/org/dbsp/formal/UnaryOperator.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2021 VMware, Inc.
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.dbsp.formal;
 
 import org.dbsp.algebraic.TimeFactory;
@@ -44,7 +67,7 @@ public abstract class UnaryOperator<T, S> {
     /**
      * @return A lifted version of this operator.
      */
-    public <F extends TimeFactory> UnaryOperator<IStream<T>, IStream<S>> lift(F factory) {
+    public UnaryOperator<IStream<T>, IStream<S>> lift(TimeFactory factory) {
         return new UnaryOperator<IStream<T>, IStream<S>>(
                 new StreamType<T>(this.inputType, factory),
                 new StreamType<S>(this.outputType, factory)) {
