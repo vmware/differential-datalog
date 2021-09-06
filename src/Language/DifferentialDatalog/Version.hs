581ba9fa21dcc649d2097ca15564ad95c4759a70
Leonid Ryzhyk <lryzhyk@vmware.com>
2021-09-06 15:47:24 -0700

    Bump version to 0.48.0.
    Signed-off-by: Leonid Ryzhyk <lryzhyk@vmware.com>

--- src/Language/DifferentialDatalog/Version.hs
+++ src/Language/DifferentialDatalog/Version.hs
@@ -33,7 +33,7 @@ import Data.List
 
 -- Keep this in sync with the binary release version on github
 dDLOG_VERSION :: String
-dDLOG_VERSION = "v0.47.0"
+dDLOG_VERSION = "v0.48.0"
 
 gitInfoCwdTry :: Either String GitInfo
 gitInfoCwdTry = $$tGitInfoCwdTry
